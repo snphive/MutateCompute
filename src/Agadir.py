3763c544774e74f46db7b7b1323ecb3799857c0b
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-07-02 16:54:41 +0200

    Add class name and constructor, stub of compute method.

---
+++ src/Agadir.py
@@ -0,0 +1,9 @@
+from src.GeneralUtilityMethods import GUM
+
+class Agadir(object):
+
+    def __init__(self):
+        print('hello world agadir constructor')
+
+    def compute(self, input_fasta):
+        print('hello world agadir.compute() method')
