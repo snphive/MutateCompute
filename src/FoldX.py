a64c4aad3d75995bf432b95a3ea3255abce00aec
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-07-02 16:55:30 +0200

    Add class name and constructor, stubs of 4 public methods.

---
+++ src/FoldX.py
@@ -0,0 +1,27 @@
+from src.GeneralUtilityMethods import GUM
+
+class FoldX(object):
+
+    def __init__(self):
+        print('helloworld')
+
+
+    class Repair(object):
+
+        def __init__(self):
+            print('helloworld')
+
+    class BuildModel(object):
+
+        def __init__(self):
+            print('helloworld')
+
+    class Stability(object):
+
+        def __init__(self):
+            print('helloworld')
+
+    class AnalyseComplex(object):
+
+        def __init__(self):
+            print('helloworld')
