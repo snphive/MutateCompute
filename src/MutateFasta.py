7d531c975086352b0273a0308e17b3eb3095b4ef
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-07-02 17:18:40 +0200

    Add MutateFasta class, class name, constructor and stub for create_mutant_fastas

---
+++ src/MutateFasta.py
@@ -0,0 +1,13 @@
+from src.GeneralUtilityMethods import GUM
+
+
+class MutateFasta(object):
+
+    def __init__(self):
+        print('hello world MutateFasta constructor')
+
+    def create_mutant_fastas(self, input_fasta, mutate_all_residues, list_of_residues_to_mutate_to):
+        print('hello world MutateFasta.create_mutant_fastas() method ')
+
+
+
