94b8d67d037c4c8196e5b6adf927081c22880fe8
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-07-12 18:14:11 +0200

    test_build_list_of_mutant_names_for_foldx passes. test_execute_job is under construction. It will require mocking.

--- tests/test_buildModel.py
+++ tests/test_buildModel.py
@@ -1,11 +1,14 @@
 from unittest import TestCase
 from src.FoldX import FoldX
 from src.GeneralUtilityMethods import GUM
-
 import mock
+from mock import patch
 
 
 class TestBuildModel(TestCase):
+
+    def setUp(self):
+        print('bla')
 
     @classmethod
     def setUpClass(cls):
@@ -31,21 +34,20 @@ class TestBuildModel(TestCase):
         cls.abs_path_tests_outputs_foldx = ''
         cls.abs_path_tests_outputs_foldx_buildmodel = ''
 
-    def test_mutate_residues(self):
+    # @patch('FoldX.BuildModel.')
+    def test_execute_job(self):
+        # mock_GUM =
         input_pdb = 'RepairPDB_Test1Residue.pdb'
         write_wt_fasta_files = False
         mutant_aa_list = ['A', 'C', 'D']
-        FoldX().BuildModel().mutate_residues_of_pdb(self.abs_path_inputs, self.abs_path_outputs, input_pdb, mutant_aa_list, write_wt_fasta_files)
-        # with mock.patch('buildModel._build_list_of_mutant_names_for_foldx', return_value='list_of_mutant_names') as abc_urandom_function:
-            # assert abc_urandom(5) == 'abcpumpkins'
+        FoldX().BuildModel().mutate_residues_of_pdb(self.abs_path_tests_inputs, self.abs_path_tests_outputs, input_pdb,
+                                                    mutant_aa_list, write_wt_fasta_files)
 
-    # Note FoldX expects mutant names to have format wtaa_chain_position_mutantaa
+    # Note FoldX expects mutant names to have format wtaa_chain_position_mutantaa.
+    # Variable names for values with this format are denoted with '_fx_'
     def test_build_list_of_mutant_names_for_foldx(self):
-        input_pdb = 'RepairPDB_Test1Residue.pdb'
-        write_wt_fasta_files = False
-        pdb_name_chain_fasta_dict = GUM.extract_pdb_name_fasta_chains_from_pdb(input_pdb, self.abs_path_tests_inputs,
-                                                self.abs_path_tests_outputs_foldx_buildmodel, write_wt_fasta_files)
+        pdb_name_chain_fasta_dict = {'Test1Residue_A': 'R', 'Test1Residue_B': 'R'}
         mutant_aa_list = ['A', 'C', 'D']
         mutant_name_fx_list = FoldX().BuildModel()._build_list_of_mutant_names_for_foldx(mutant_aa_list, pdb_name_chain_fasta_dict)
-        expected_mutant_name_list = ['RA1A', 'RA1C', 'RA1D', 'RB1A', 'RB1C', 'RB1D']
-        self.assertEqual(mutant_name_fx_list, expected_mutant_name_list)
+        expected_mutant_name_fx_list = ['RA1A', 'RA1C', 'RA1D', 'RB1A', 'RB1C', 'RB1D']
+        self.assertEqual(mutant_name_fx_list, expected_mutant_name_fx_list)
