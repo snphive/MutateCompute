d11da759da9509f90b68acee9ff33cf3373c078b
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-07-20 19:03:56 +0200

    Replace troublesome os.path.join with bespoke method. Improve numerous var names. Call GUM's method directly, remove intermediary self.method.

--- src/FoldX.py
+++ src/FoldX.py
@@ -21,84 +21,66 @@ class FoldX(object):
 
         def __init__(self, path_zeus_foldx_exe, path_local_foldx_exe):
             print('buildmodel constructor')
-            self.foldx_buildmodel_job_prefix: str = 'FXBM_'
-            self.path_zeus_foldx_exe: str = path_zeus_foldx_exe
-            self.path_local_foldx_exe: str = path_local_foldx_exe
+            self.FXBM_jobname_prefix = 'FXBM_'
+            self.path_zeus_foldx_exe = path_zeus_foldx_exe
+            self.path_local_foldx_exe = path_local_foldx_exe
 
         # Mutate specified amino acids in this pdb to all listed amino acids using FoldX BuildModel,
         # FoldX uses a runscript file, which must be written here.
-        # abs_path_inputs          Absolute path of Inputs folder.
-        # abs_path_outputs         Absolute path of Outputs folder.
-        # input_pdb                Input pdb to be mutated.
-        # mutate_to_aa_list        List of amino acids that you want to mutate your pdb to.
-        # write_wt_fasta_files     True/False is you want to write the wild-type sequence of the input pdb out.
-        def mutate_residues_of_pdb(self, abs_path_inputs, abs_path_outputs, input_pdb, mutate_to_aa_list,
+        # path_Inputs          String   Absolute path of Inputs folder. (Can be local or cluster).
+        # path_Outputs         String   Absolute path of Outputs folder. (Can be local or cluster).
+        # input_pdb            String   Input pdb to be mutated.
+        # mutate_to_aa_list    List     List of amino acids that you want to mutate your pdb to.
+        # write_wt_fasta_files Boolean  True/False is you want to write the wild-type sequence of the input pdb out.
+        def mutate_residues_of_pdb(self, path_Inputs, path_Outputs, pdb, mutate_to_aa_list,
                                    write_wt_fasta_files):
-            pdb_name_chain_fasta_dict = GUM.extract_pdb_name_fasta_chains_from_pdb(input_pdb, abs_path_inputs,
-                                                                            abs_path_outputs, write_wt_fasta_files)
-            mutant_name_fx_list = self._make_list_of_mutant_names_for_foldx(mutate_to_aa_list,
-                                                                            pdb_name_chain_fasta_dict)
-            runscript_dest_folder = GUM.create_dir_tree(abs_path_inputs + '/FoldX/BuildModel')
-            self._write_runscript_for_FoldX_BuildModel(input_pdb, runscript_dest_folder)
-            abs_path_outputs_FX_BM_pdb = self._create_foldx_buildmodel_pdb_dir_tree(abs_path_outputs, input_pdb)
-            os.chdir(abs_path_outputs_FX_BM_pdb)
+            pdbname = pdb.split('.')[0]
+            path_Inputs_PDBs_pdbname = GUM.create_dir_tree(path_Inputs, 'PDBs', pdbname)
+            pdbname_chain_fasta_dict = GUM.extract_pdbname_chain_fasta_from_pdb(pdb, path_Inputs, path_Outputs,
+                                                                                  write_wt_fasta_files)
+            fx_mutant_name_list = self._make_fx_mutant_name_list(mutate_to_aa_list,
+                                                                 pdbname_chain_fasta_dict)
+            path_runscript_dest = GUM.create_dir_tree(path_Inputs_PDBs_pdbname, 'FX_BuildModel')
 
-            for mutant_name_fx in mutant_name_fx_list:
-                abs_path_outputs_FX_BM_pdb_mutant = GUM.create_dir_tree(abs_path_outputs_FX_BM_pdb, mutant_name_fx)
+            if not os.path.exists(path_runscript_dest):
+                os.makedirs(path_runscript_dest)
+
+            action = '<BuildModel>#,individual_list.txt'
+            GUM.write_runscript_for_pdbs(path_runscript_dest, 'RepairPDB_' + pdb, action)
+
+            for fx_mutant_name in fx_mutant_name_list:
+                path_jobq_indivlist_dest = GUM.create_dir_tree(path_Inputs_PDBs_pdbname, fx_mutant_name)
+
+                if not os.path.exists(path_jobq_indivlist_dest):
+                    os.makedirs(path_jobq_indivlist_dest)
                 try:
-                    os.chdir(abs_path_outputs_FX_BM_pdb_mutant)
+                    os.chdir(path_jobq_indivlist_dest)
                 except ValueError:
                     print('directory tree was not created')
 
-                self._write_individual_list_for_mutant(mutant_name_fx, abs_path_outputs_FX_BM_pdb_mutant)
-                no_python_script = ''
+                self._write_individual_list_for_mutant(fx_mutant_name, path_jobq_indivlist_dest)
                 # path_foldx = self.path_zeus_foldx_exe if use_cluster else self.path_local_foldx_exe
-                self._write_job_q_bash_to_run_on_cluster_using_runscript(mutant_name_fx, no_python_script)
+                job_name = self.FXBM_jobname_prefix + fx_mutant_name
+                Cluster.write_job_q_bash(job_name, path_jobq_indivlist_dest)
 
-                if os.path.isfile('./runscript.txt'):
+                if os.path.exists(path_runscript_dest + '/' + 'runscript.txt'):
                     subprocess.call('qsub job.q', shell=True)
                 else:
                     raise ValueError('No runscript file was found')
 
-        def _create_foldx_buildmodel_pdb_dir_tree(self, abs_path_outputs, pdb):
-            return GUM.create_dir_tree(abs_path_outputs, 'FoldX', 'BuildModel', pdb.split('.')[0])
-
-        # mutant_name_fx            FoldX-specific format for a mutant:  wtaa_chain_position_mutantaa.
-        # python_script_with_paths  python script (including .py extension) to run, preceded by path to script
-        #                           This can be followed by any paths that the script will receive via sys.argv.
-        # abs_path_job_q_file       Destination folder for the job.q file being written here. Typically this will be
-        #                           in Inputs/Options/Agadir folder or Inputs/PDBs/<pdb>/<mutant_fx>/Cluster folder
-        def _write_job_q_bash_to_run_on_cluster_using_runscript(self, mutant_name_fx, python_script_with_paths,
-                                                                abs_path_job_q_file):
-            job_name = self.foldx_buildmodel_job_prefix + mutant_name_fx
-            using_runscript = True
-            return Cluster.write_job_q_bash(job_name, using_runscript, python_script_with_paths, abs_path_job_q_file)
-
-        # Note FoldX expects mutant names to have format wtaa_chain_position_mutantaa.
-        # Variable names for values with this format are denoted with '_fx_'
-        def _make_list_of_mutant_names_for_foldx(self, mutate_to_aa_list, pdb_name_chain_fasta_dict):
-            mutant_name_fx_list = []
-            for pdb_name_chain, fasta_sequence in pdb_name_chain_fasta_dict.items():
-                chain = pdb_name_chain.split('_')[-1]
+        def _make_fx_mutant_name_list(self, mutate_to_aa_list, pdbname_chain_fasta_dict):
+            fx_mutant_name_list = []
+            for pdbname_chain, fasta_sequence in pdbname_chain_fasta_dict.items():
+                chain = pdbname_chain.split('_')[-1]
                 for index, wt_aa in enumerate(fasta_sequence):
                     position = index + 1
                     for mutant_aa in mutate_to_aa_list:
-                        mutant_name_fx_list.append(wt_aa + chain + str(position) + mutant_aa)
-            return mutant_name_fx_list
+                        fx_mutant_name_list.append(wt_aa + chain + str(position) + mutant_aa)
+            return fx_mutant_name_list
 
-        # Note FoldX expects the mutant_name to have the form: WTaa_chain_position_mutantaa (no gaps or underscores)
-        def _write_runscript_for_FoldX_BuildModel(self, name_of_repair_pdb, abs_path_runscript='./'):
-            repair_pdbs = 'RepairPDB_' + name_of_repair_pdb + '.pdb'
-            show_sequence_detail = False
-            action = '<BuildModel>#,individual_list.txt'
-            print_networks = False
-            calculate_stability = False
-            GUM.write_runscript_for_pdbs(abs_path_runscript, repair_pdbs, show_sequence_detail, action, print_networks,
-                                                          calculate_stability)
-
-        def _write_individual_list_for_mutant(self, mutant_name, absolute_path_output):
-            individual_list_for_this_mutant_only = open(absolute_path_output + 'individual_list.txt', 'w')
-            individual_list_for_this_mutant_only.write(mutant_name + ';\n')
+        def _write_individual_list_for_mutant(self, fx_mutant_name, path_indivlist_dest):
+            individual_list_for_this_mutant_only = open(path_indivlist_dest + 'individual_list.txt', 'w')
+            individual_list_for_this_mutant_only.write(fx_mutant_name + ';\n')
             individual_list_for_this_mutant_only.close()
 
     class Stability(object):
@@ -111,24 +93,19 @@ class FoldX(object):
         def __init__(self):
             print('helloworld constructor')
 
-        def _prepare_for_FoldX_AnalyseComplex(self, repair_pdb_name):
+        def _prepare_for_FoldX_AnalyseComplex(self, repair_pdbname):
             _0_1_2_pdbs = ['0.pdb,', '1.pdb,', '2.pdb,']
-            repair_pdb_name_1_ = repair_pdb_name + '_1_'
-            wt_repair_pdb_name_1_ = 'WT_' + repair_pdb_name_1_
+            repair_pdbname_1_ = repair_pdbname + '_1_'
+            wt_repair_pdbname_1_ = 'WT_' + repair_pdbname_1_
 
             path_to_runscript = './'
-            pdbs_to_analyse = repair_pdb_name_1_ + _0_1_2_pdbs[0] + \
-                              repair_pdb_name_1_ + _0_1_2_pdbs[1] + \
-                              repair_pdb_name_1_ + _0_1_2_pdbs[2] + \
-                              wt_repair_pdb_name_1_ + _0_1_2_pdbs[0] + \
-                              wt_repair_pdb_name_1_ + _0_1_2_pdbs[1] + \
-                              wt_repair_pdb_name_1_ + _0_1_2_pdbs[2]
-            show_sequence_detail = False
+            pdbs_to_analyse = repair_pdbname_1_ + _0_1_2_pdbs[0] + \
+                              repair_pdbname_1_ + _0_1_2_pdbs[1] + \
+                              repair_pdbname_1_ + _0_1_2_pdbs[2] + \
+                              wt_repair_pdbname_1_ + _0_1_2_pdbs[0] + \
+                              wt_repair_pdbname_1_ + _0_1_2_pdbs[1] + \
+                              wt_repair_pdbname_1_ + _0_1_2_pdbs[2]
             action = '<AnalyseComplex>#'
-            print_networks = False
-            calculate_stability = False
-            GUM.write_runscript_for_pdbs(path_to_runscript, pdbs_to_analyse,
-                                         show_sequence_detail, action, print_networks,
-                                         calculate_stability)
+            GUM.write_runscript_for_pdbs(path_to_runscript, pdbs_to_analyse, action)
 
 
