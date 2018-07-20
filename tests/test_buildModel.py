87ba3c07cf3e4242b90104f7dda6272f2be39ad0
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-07-20 19:07:56 +0200

    Improved and debugged tests and code. The tests written currently pass.

--- tests/test_buildModel.py
+++ tests/test_buildModel.py
@@ -1,42 +1,40 @@
 from unittest import TestCase
 from src.FoldX import FoldX
 from src.GeneralUtilityMethods import GUM
+from src.Cluster import Cluster
 from unittest.mock import patch
-import shutil
-import os
+from tests.testHelperMethods import THM
 
+
+# Note: A test pdb is used here which is taken from the RepairPDBs folder, but includes only the first 5 residues of
+# to facilitate manual creation of test variables, such as the list of all mutants.
+# Note: Wherever a path is indicated, it is the absolute path. Relative paths are always prefixed with rel_
 class TestBuildModel(TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.abs_path_local = '/Users/u0120577/PycharmProjects/MutateCompute'
-        cls.rel_path_tests_inputs = 'tests/Inputs'
-        cls.rel_path_tests_outputs = 'tests/Outputs'
-        cls.abs_path_local_tests = '/Users/u0120577/PycharmProjects/MutateCompute/tests'
-        cls.abs_path_local_tests_inputs = cls.abs_path_local_tests + '/Inputs'
-        cls.abs_path_local_tests_inputs_foldx = cls.abs_path_local_tests_inputs + '/FoldX'
-        cls.abs_path_local_tests_inputs_fasta = cls.abs_path_local_tests_inputs + '/Fasta'
-        cls.abs_path_local_tests_inputs_pdbs = cls.abs_path_local_tests_inputs + '/PDBs'
-        cls.abs_path_local_tests_outputs = cls.abs_path_local_tests + '/Outputs'
-        cls.abs_path_local_tests_outputs_agadir = cls.abs_path_local_tests_outputs + '/Agadir'
-        cls.abs_path_local_tests_outputs_fasta = cls.abs_path_local_tests_outputs + '/Fasta'
-        cls.abs_path_local_tests_outputs_foldx = cls.abs_path_local_tests_outputs + '/FoldX'
-        cls.abs_path_local_tests_outputs_foldx_buildmodel = cls.abs_path_local_tests_outputs_foldx + '/BuildModel'
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.abs_local_path_tests = ''
-        cls.abs_path_local_tests_inputs = ''
-        cls.abs_path_local_tests_inputs_foldx = ''
-        cls.abs_path_local_tests_inputs_fasta = ''
-        cls.abs_path_local_tests_inputs_pdbs = ''
-        cls.abs_path_local_tests_outputs = ''
-        cls.abs_path_local_tests_outputs_agadir = ''
-        cls.abs_path_local_tests_outputs_fasta = ''
-        cls.abs_path_local_tests_outputs_foldx = ''
-        cls.abs_path_local_tests_outputs_foldx_buildmodel = ''
+        # paths common to both input & output
+        cls.path_tests = '/Users/u0120577/PycharmProjects/MutateCompute/tests'
+        cls.rel_path_PDBs = '/PDBs'
+        cls.rel_path_FoldX = '/FoldX'
+        # input paths only
+        cls.rel_path_Inputs = '/Inputs'
+        cls.path_tests_Inputs = cls.path_tests + cls.rel_path_Inputs
+        cls.path_tests_Inputs_PDBs = cls.path_tests_Inputs + cls.rel_path_PDBs
+        cls.rel_path_Cluster = '/Cluster'
+        cls.rel_path_BuildModel = '/BuildModel'
+        cls.rel_path_Fasta = '/Fasta'
+        cls.rel_path_Options_Agadir = '/Options/Agadir'
+        cls.rel_path_Options_FoldX = '/Options/FoldX'
+        cls.rel_path_Options_Cluster = '/Options/Cluster'
+        # output paths only
+        cls.rel_path_Outputs = '/Outputs'
+        cls.path_tests_Outputs = cls.path_tests + cls.rel_path_Outputs
+        cls.rel_path_BuildModel = '/BuildModel'
+        cls.rel_path_AnalyseComplex = '/AnalyseComplex'
 
     def setUp(self):
+        # THM.remove_tests_Inputs_Outputs_folders()
         self.path_zeus_foldx_exe = '/switchlab/group/tools/FoldX_2015/FoldX'
         self.path_local_foldx_exe = '/Users/u0120577/SNPEFFECT/executables/FoldX'
         foldx = FoldX()
@@ -44,105 +42,59 @@ class TestBuildModel(TestCase):
 
     def tearDown(self):
         self.buildModel = None
-        self._remove_tests_inputs_outputs_folders()
+        # THM.remove_tests_Inputs_Outputs_folders()
 
-    def _remove_tests_inputs_outputs_folders(self):
-        if os.path.exists(self.abs_path_local_tests):
-            if os.path.exists(self.abs_path_local_tests_inputs):
-                TestBuildModel.__delete_directory_tree_of_tests_inputs_outputs(self.rel_path_tests_inputs)
-            if os.path.exists(self.abs_path_local_tests_outputs):
-                TestBuildModel.__delete_directory_tree_of_tests_inputs_outputs(self.rel_path_tests_outputs)
-
-    @staticmethod
-    def __delete_directory_tree_of_tests_inputs_outputs(tests_inputs_outputs):
-        os.chdir(TestBuildModel.abs_path_local)
-        print(os.getcwd())
-        if not os.getcwd() == '/Users/u0120577/PycharmProjects/MutateCompute':
-            raise ValueError('Current working directory is not /MutateCompute. Not proceeding with deletion of folder')
-        else:
-            try:
-                shutil.rmtree(tests_inputs_outputs)
-            except OSError as e:
-                print("Error removing: %s - %s." % (e.filename, e.strerror))
-
-    @patch.object(FoldX.BuildModel, '_write_job_q_bash_to_run_on_cluster_using_runscript')
+    @patch('subprocess.call')
+    @patch.object(Cluster, 'write_job_q_bash')
     @patch.object(FoldX.BuildModel, '_write_individual_list_for_mutant')
     @patch('os.chdir')
-    @patch.object(FoldX.BuildModel, '_create_foldx_buildmodel_pdb_dir_tree')
-    @patch.object(FoldX.BuildModel, '_write_runscript_for_FoldX_BuildModel')
-    @patch.object(FoldX.BuildModel, '_make_list_of_mutant_names_for_foldx')
-    @patch.object(GUM, 'extract_pdb_name_fasta_chains_from_pdb')
-    def test_mutate_residues_of_pdb(self, mock_extract_pdb_name_fasta_chains_from_pdb,
-                                    mock_make_list_of_mutant_names_for_foldx,
-                                    mock_write_runscript_for_FoldX_BuildModel,
-                                    mock_create_foldx_buildmodel_pdb_dir_tree,
+    @patch.object(GUM, 'create_dir_tree')
+    @patch.object(GUM, 'write_runscript_for_pdbs')
+    @patch.object(FoldX.BuildModel, '_make_fx_mutant_name_list')
+    @patch.object(GUM, 'extract_pdbname_chain_fasta_from_pdb')
+    def test_mutate_residues_of_pdb(self,
+                                    mock_extract_pdbname_chain_fasta_from_pdb,
+                                    mock_make_fx_mutant_name_list,
+                                    mock_write_runscript_for_pdbs,
+                                    mock_create_dir_tree,
                                     mock_os_chdir,
                                     mock_write_individual_list_for_mutant,
-                                    mock__write_job_q_bash_to_run_on_cluster_using_runscript):
+                                    mock_write_job_q_bash,
+                                    mock_subprocess_call):
         # arrange
-        mock_extract_pdb_name_fasta_chains_from_pdb.return_value = {'RepairPDB_Test1Residue_A': 'R',
-                                                                    'RepairPDB_Test1Residue_B': 'R'}
-        mock_make_list_of_mutant_names_for_foldx.return_value = ['RA1A', 'RA1C', 'RA1D', 'RB1A', 'RB1C', 'RB1D']
-        input_pdb = 'RepairPDB_Test1Residue.pdb'
-        input_pdb_name = input_pdb.split('_')[-1].split('.')[0]
-        mock_create_foldx_buildmodel_pdb_dir_tree.return_value = self.abs_path_local_tests_outputs_foldx_buildmodel + '/' + \
-                                                                 input_pdb_name
+        mock_extract_pdbname_chain_fasta_from_pdb.return_value = {'RepairPDB_1_A': 'RVYLT', 'RepairPDB_1_B': 'RVYLT'}
+        # Note FoldX expects mutant names to have format wtaa_chain_position_mutantaa.
+        mock_make_fx_mutant_name_list.return_value = ['RA1A', 'RA1C', 'VA2A', 'VA2C', 'YA3A', 'YA3C', 'LA4A',
+                                                          'LA4C', 'TA5A', 'TA5C', 'RB1A', 'RB1C', 'VB2A', 'VB2C',
+                                                          'YB3A', 'YB3C', 'LB4A', 'LB4C', 'TB5A', 'TB5C']
+        pdb = 'RepairPDB_1.pdb'
+
         mock_os_chdir.return_value = None
         write_wt_fasta_files = False
-        mutant_aa_list = ['A', 'C', 'D']
+        mutant_aa_list = ['A', 'C']
         # action
-        self.buildModel.mutate_residues_of_pdb(self.abs_path_local_tests_inputs, self.abs_path_local_tests_outputs, input_pdb,
-                                               mutant_aa_list, write_wt_fasta_files)
+        self.buildModel.mutate_residues_of_pdb(self.path_tests_Inputs, self.path_tests_Outputs, pdb, mutant_aa_list,
+                                               write_wt_fasta_files)
         # assert
         expected_call_count_1 = 1
-        expected_call_count_6 = 6
-        self.assertEqual(expected_call_count_1, mock_extract_pdb_name_fasta_chains_from_pdb.call_count)
-        self.assertEqual(expected_call_count_1, mock_make_list_of_mutant_names_for_foldx.call_count)
-        self.assertEqual(expected_call_count_1, mock_write_runscript_for_FoldX_BuildModel.call_count)
-        self.assertEqual(expected_call_count_1, mock_create_foldx_buildmodel_pdb_dir_tree.call_count)
-        self.assertEqual(expected_call_count_6, mock_write_individual_list_for_mutant.call_count)
-        self.assertEqual(expected_call_count_6, mock__write_job_q_bash_to_run_on_cluster_using_runscript.call_count)
+        expected_call_count_20 = len(mock_make_fx_mutant_name_list.return_value)
+        expected_call_count_22 = 22
+        self.assertEqual(expected_call_count_1, mock_extract_pdbname_chain_fasta_from_pdb.call_count)
+        self.assertEqual(expected_call_count_1, mock_make_fx_mutant_name_list.call_count)
+        self.assertEqual(expected_call_count_1, mock_write_runscript_for_pdbs.call_count)
+        self.assertEqual(expected_call_count_22, mock_create_dir_tree.call_count)
+        self.assertEqual(expected_call_count_20, mock_write_individual_list_for_mutant.call_count)
+        self.assertEqual(expected_call_count_20, mock_write_job_q_bash.call_count)
+        self.assertEqual(expected_call_count_20, mock_subprocess_call.call_count)
 
     # Note FoldX expects mutant names to have format wtaa_chain_position_mutantaa.
     # Variable names for values with this format are denoted with '_fx_'
-    def test_make_list_of_mutant_names_for_foldx(self):
-        pdb_name_chain_fasta_dict = {'Test1Residue_A': 'R', 'Test1Residue_B': 'R'}
+    def test_make_fx_mutant_name_list(self):
+        pdbname_chain_fasta_dict = {'Test1Residue_A': 'R', 'Test1Residue_B': 'R'}
         mutant_aa_list = ['A', 'C', 'D']
-        mutant_name_fx_list = self.buildModel._make_list_of_mutant_names_for_foldx(mutant_aa_list, pdb_name_chain_fasta_dict)
-        expected_mutant_name_fx_list = ['RA1A', 'RA1C', 'RA1D', 'RB1A', 'RB1C', 'RB1D']
-        self.assertEqual(mutant_name_fx_list, expected_mutant_name_fx_list)
-
-    def test_write_job_q_bash_to_run_on_cluster_using_runscript_no_queue_slots_memlimit_or_clusternode(self):
-        # arrange
-        foldx_buildmodel_job_prefix = 'FXBM_'
-        mutant_name_fx = 'RA1A'
-        job_name = foldx_buildmodel_job_prefix + mutant_name_fx
-        python_script_with_path = '/somepath/to/python/script.py'
-        abs_path_job_q_file = self.abs_path_local_tests_inputs_foldx
-        expected_job_q = '#!/bin/bash\n' + '#$ -N ' + job_name + '\n' + '#$ -V\n' + '#$ -cwd\n' + \
-                              'source ~/.bash_profile\n' + self.path_zeus_foldx_exe + ' -runfile runscript.txt\n' + \
-                              'python ' + python_script_with_path + '\n'
-        single_space = ' '
-        not_expected_job_q = '#!/bin/bash' + single_space + '\n' + '#$ -N ' + job_name + '\n' + '#$ -V\n' + \
-                             '#$ -cwd\n' + 'source ~/.bash_profile\n' + self.path_zeus_foldx_exe + \
-                             ' -runfile runscript.txt\n' + 'python ' + python_script_with_path + '\n'
-        missing_new_line = ''
-        not_expected_job_q_2 = '#!/bin/bash\n' + '#$ -N ' + job_name + missing_new_line + '#$ -V\n' + \
-                             '#$ -cwd\n' + 'source ~/.bash_profile\n' + self.path_zeus_foldx_exe + \
-                             ' -runfile runscript.txt\n' + 'python ' + python_script_with_path + '\n'
-        # action
-        actual_job_q = self.buildModel._write_job_q_bash_to_run_on_cluster_using_runscript(mutant_name_fx,
-                                                                        python_script_with_path, abs_path_job_q_file)
-        # assert
-        self.assertEqual(actual_job_q, expected_job_q)
-        self.assertNotEqual(actual_job_q, not_expected_job_q)
-        self.assertNotEqual(actual_job_q, not_expected_job_q_2)
-        self._test_job_q_bash_file_created(abs_path_job_q_file)
-
-    def _test_job_q_bash_file_created(self, abs_path_job_q):
-        self.assertTrue(os.path.exists(abs_path_job_q), 'path to job.q path does not exist: ' + abs_path_job_q)
-        if os.path.exists(abs_path_job_q):
-            self.assertTrue(os.path.isfile('job.q'))
+        fx_mutant_name_list = self.buildModel._make_fx_mutant_name_list(mutant_aa_list, pdbname_chain_fasta_dict)
+        expected_fx_mutant_name_list = ['RA1A', 'RA1C', 'RA1D', 'RB1A', 'RB1C', 'RB1D']
+        self.assertEqual(fx_mutant_name_list, expected_fx_mutant_name_list)
 
 
 
@@ -152,8 +104,15 @@ class TestBuildModel(TestCase):
 
     # unused file comparison which relies to some extent on the directory structure being in place which I didn't know how to mock
     # so I made the write_job.. method return the string that it writes to job.q file.
-    #     with open(abs_path_job_q_file + '/job.q') as f:
+    #     with open(path_job_q_file + '/job.q') as f:
     #         actual_job_q = f.read()
     #         self.assertEqual(actual_job_q, expected_job_q)
     #         self.assertNotEqual(actual_job_q, not_expected_job_q)
     #         self.assertNotEqual(actual_job_q, not_expected_job_q_2)
+
+    # pdbname = pdb.split('.')[0]
+    # path_tests_Inputs_PDBs_pdbname = self.path_tests_Inputs_PDBs + '/' + pdbname
+    # path_tests_Inputs_PDBs_pdbname_FX_BM = path_tests_Inputs_PDBs_pdbname + '/FX_BuildModel'
+    # path_tests_Inputs_PDBs_pdbname_FXmutantname = path_tests_Inputs_PDBs_pdbname + '/RA1A'
+    # mock_os_path_join.side_effect = [path_tests_Inputs_PDBs_pdbname, path_tests_Inputs_PDBs_pdbname_FX_BM,
+    #                                  path_tests_Inputs_PDBs_pdbname_FXmutantname]
