da10c3148817576b90fadbe2af09723792a8fc4f
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-07-24 19:07:23 +0200

    Complete test and method to retrieve a dictionary of blast hits that gives 100 % identity with zero gaps.  _make_alignmentDict...() method not completely finished (has a small bug)

--- src/Biopython.py
+++ src/Biopython.py
@@ -1,4 +1,61 @@
+# Biopython packages NCBIWWW and NCBIXML are used here to perform a blast of query fasta sequences and to parse the
+# resulting output xml file, respectively.
+#######################################################################################################################
+# NCBIXML parses the xml to an object called blast_record which has 49 attributes:
+#
+# blast_record
+#   |
+#   alignments
+#   |
+#   database
+#   |
+#   database_sequences
+#   |
+#   query_length
+#   |
+#   query_letters
+#######################################################################################################################
+# Nested in alignments are the following attributes:
+#
+# <Alignment> (unnamed element in zero-index-based list of alignments)
+#   |
+#   accession (string, identifier, e.g. Q99985)
+#   |
+#   hit_def (name of protein hit; alternative names)
+#   |
+#   hit_id (different identifiers for protein hit)
+#   |
+#   hsps (list of high scoring pairs)
+#   |
+#   length (full length of protein hit, not the query protein)
+#   |
+#   title (concatenation of hit_id and hit_def)
+#######################################################################################################################
+# Nested in each element of hsps list are 17 attributes, including the following:
+#
+# <HSP> (unnamed element in zero-index-based list of hsps)
+#   |
+#   align_length (int, length of this alignment.)
+#   |
+#   expect (float, 0-to-1 score that indicates how unlikely-to-likely this query sequence is to produce a random hit)
+#   |
+#   gaps (int, gaps in the alignment)
+#   |
+#   identities (int, number of identical residues)
+#   |
+#   positives (int, either identical or similar residues)
+#   |
+#   query_end (position of end of this alignment on query sequence)
+#   |
+#   query_start (position of start of this alignment on query sequence)
+#   |
+#   sbjct_end (position of end of this alignment on hit sequence)
+#   |
+#   sbjct_start (position of start of this alignment on hit sequence)
+#
+#######################################################################################################################
 import os
+import time
 from Bio.Blast import NCBIWWW
 from Bio.Blast import NCBIXML
 from Bio import SeqIO
@@ -7,8 +64,8 @@ from src.GeneralUtilityMethods import GUM
 
 class Biopy(object):
 
-    PATH_OUTPUT_LEAF = 'blastp_swissprot'
-    MILLION_BYTES = 1000000
+    PATH_OUTPUT_LEAF = "blastp_swissprot"
+    TWO_HUNDRED_KB = 200000
 
     @staticmethod
     def _read_input_fasta_seq(path_fasta_input_query_seq):
@@ -19,55 +76,102 @@ class Biopy(object):
     @staticmethod
     def _write_qblast_xml_result(qblast_result, path_output_root, path_output_leaf, output_filename):
         path_output = GUM.create_dir_tree(path_output_root, path_output_leaf)
-        path_result_file = path_output + '/' + output_filename + '.xml'
+        path_result_file = path_output + "/" + output_filename + ".xml"
         with open(path_result_file, 'w') as out_handle:
             out_handle.write(qblast_result.read())
             qblast_result.close()
         return path_result_file
 
-    # NOTE: time taken for this remote qblast for sequence 1_A.fasta was about 20 seconds.
+    # NOTE: Time taken for this remote qblast for sequence 1_A.fasta was about 20 seconds.
+    # NOTE: The query sequence id is also the filename and is used here for the name of the output xml.
     @staticmethod
     def find_identical_blastp_hit_swissprot_for_fasta_input(path_fasta_input_query_seq, path_output_root):
         fasta_input_seq = Biopy._read_input_fasta_seq(path_fasta_input_query_seq)
+        start = time.time()
         qblast_result = Biopy._run_blastp(fasta_input_seq)
-        output_filename = path_fasta_input_query_seq.split('/')[-1].split('.')[0]
+        end = time.time()
+        print(end - start)
+        filename = path_fasta_input_query_seq.split("/")[-1].split('.')[0]
         path_qblast_result = Biopy._write_qblast_xml_result(qblast_result, path_output_root, Biopy.PATH_OUTPUT_LEAF,
-                                                              output_filename)
-        minimum_filesize_for_parsing = Biopy.MILLION_BYTES
-
+                                                              filename)
+        minimum_filesize_for_parsing = Biopy.TWO_HUNDRED_KB
         qblast_xml_size = os.stat(path_qblast_result).st_size
+        if qblast_xml_size > minimum_filesize_for_parsing:
+            raise ValueError("Qblast xml output size is over 200 KB in size. Something may be wrong.")
 
-        if qblast_xml_size < minimum_filesize_for_parsing:
-            with open(path_qblast_result) as f:
-                blast_record = NCBIXML.read(f)
-        else:
-            with open(path_qblast_result) as f:
-                blast_records = NCBIXML.parse(f)
+        with open(path_qblast_result) as f:
+            blast_record = NCBIXML.read(f)
+            query_length = blast_record.query_length
+            query_letters = blast_record.query_letters
+            database_used = blast_record.database
+            database_seqs_num = blast_record.database_sequences
+            alignments = blast_record.alignments
 
-        if not blast_record.BlastOutput.BlastOutput_query-len == len(SeqIO.read(fasta_input_seq, 'fasta').seq):
-            raise ValueError('The length of the fasta input sequence is different from the sequence length output by Blast - Something has gone wrong')
+        qblast_result_dict = {'query_seq_id': filename,
+                              'query_length': query_length,
+                              'database': database_used,
+                              'database_seqs_num': database_seqs_num,
+                              'alignment_dict': Biopy._make_alignmentDict_zeroGaps_queryLen_equal_alignLen(query_length,
+                                                                                                           alignments)}
 
+        Biopy.__print_discrepancies_in_query_sequence_length(query_length, query_letters, path_fasta_input_query_seq)
+        return qblast_result_dict
 
-        for hit in blast_record.Hit:
-            for hsp in hit.Hsp:
-                print('Hsp_identity: ' + hsp.Hsp_identity)
-                print('Hsp_gaps: ' + hsp.Hsp_gaps)
-
-        blast_record.close()
-        blast_records.close()
-        return 'bla'
-
+    # Using alignments restricted to 5 and hitlist restricted to 5, the time qblast takes to run sequence 1_A.fasta
+    # remotely is only reduced from 22 to 21 seconds. However, the output xml is reduced in size from 77 KB to 15 KB.
+    # The remaining data blast_record are not used so nothing is lost by reducing the scope of the analysis. We are
+    # only looking for 100 % identity anyway.
+    # NOTE: occasionally the qblast took over 4 minutes!
     @staticmethod
     def _run_blastp(fasta_input_seq):
         return NCBIWWW.qblast(program=Biopy.BlastParam.BLST_P.value,
                               database=Biopy.BlastParam.SWSPRT.value,
                               sequence=fasta_input_seq,
-                              entrez_query=Biopy.BlastParam.HOMSAP_ORG.value)
+                              entrez_query=Biopy.BlastParam.HOMSAP_ORG.value,
+                              alignments=Biopy.BlastParam.MAX_ALIGN_20.value,
+                              hitlist_size=Biopy.BlastParam.MAX_HIT_20.value)
 
+    # @staticmethod
+    # def _zero_gaps_query_length_equals_align_length(qblast_result_dict):
+    #     if qblast_result_dict['alignment']['hsp']['gaps'] == 0 and
+
+    @staticmethod
+    def _make_alignmentDict_zeroGaps_queryLen_equal_alignLen(query_length, alignments):
+        alignment_dict = {'hsp_dict': {}}
+        for alignment in alignments:
+            for hsp in alignment.hsps:
+                if hsp.expect == 0.0 and hsp.gaps == 0 and query_length == hsp.align_length:
+                    alignment_dict['accession_num'] = alignment.accession
+                    alignment_dict['length'] = alignment.length
+                    alignment_dict['hit_def'] = alignment.hit_def
+                    alignment_dict['hsp_dict']['align_length'] = hsp.align_length
+                    alignment_dict['hsp_dict']['gaps'] = hsp.gaps
+                    alignment_dict['hsp_dict']['identities'] = hsp.identities
+                    alignment_dict['hsp_dict']['query_end'] = hsp.query_end
+                    alignment_dict['hsp_dict']['query_start'] = hsp.query_start
+                    alignment_dict['hsp_dict']['sbjct_end'] = hsp.sbjct_end
+                    alignment_dict['hsp_dict']['sbjct_start'] = hsp.sbjct_start
+            for name, value in alignment_dict.items():
+                print(name + "  " + str(value))
+        return alignment_dict
+
+
+    @staticmethod
+    def __print_discrepancies_in_query_sequence_length(query_length, query_letters, path_fasta_input_query_seq):
+        if not query_length == query_letters:
+            print("query_length of blast_record is not same as query letters of blast_record")
+
+        if not query_length == len(SeqIO.read(path_fasta_input_query_seq, 'fasta').seq):
+            print("Length of fasta input sequence not same as query length of blast_record")
+
+# Note this enum class is created to hold constant values.
     from enum import Enum
 
     class BlastParam(Enum):
-        BLST_P = 'blastp'
-        SWSPRT = 'swissprot'
-        HOMSAP_ORG = 'Homo Sapiens[Organism]'
-
+        BLST_P = "blastp"
+        SWSPRT = "swissprot"
+        HOMSAP_ORG = "Homo Sapiens[Organism]"
+        MAX_ALIGN_5 = 5
+        MAX_HIT_5 = 5
+        MAX_ALIGN_20 = 20
+        MAX_HIT_20 = 20
--- tests/test_biopython.py
+++ tests/test_biopython.py
@@ -7,44 +7,41 @@ from unittest.mock import patch
 class TestBiopython(TestCase):
 
     def setUp(self):
-        self.path_fasta_input_test_seq1 = '/Users/u0120577/ROB_HOMOLOGY_HUMAN/Fasta/1_A.fasta'
-        self.path_fasta_input_test_seq2 = '/Users/u0120577/ROB_HOMOLOGY_HUMAN/Fasta/1_B.fasta'
-        self.path_fasta_input_test_seq3 = '/Users/u0120577/ROB_HOMOLOGY_HUMAN/Fasta/2_A.fasta'
-        self.path_fasta_input_test_seq4 = '/Users/u0120577/ROB_HOMOLOGY_HUMAN/Fasta/3_A.fasta'
-        self.path_fasta_input_test_seq5 = '/Users/u0120577/ROB_HOMOLOGY_HUMAN/Fasta/3_B.fasta'
+        self.path_fasta_input_input_query_seq1 = "/Users/u0120577/ROB_HOMOLOGY_HUMAN/Fasta/1_A.fasta"
+        self.path_fasta_input_input_query_seq2 = "/Users/u0120577/ROB_HOMOLOGY_HUMAN/Fasta/1_B.fasta"
+        self.path_fasta_input_input_query_seq3 = "/Users/u0120577/ROB_HOMOLOGY_HUMAN/Fasta/2_A.fasta"
+        self.path_fasta_input_input_query_seq4 = "/Users/u0120577/ROB_HOMOLOGY_HUMAN/Fasta/3_A.fasta"
+        self.path_fasta_input_input_query_seq5 = "/Users/u0120577/ROB_HOMOLOGY_HUMAN/Fasta/3_B.fasta"
 
-        self.path_output_root = '/Users/u0120577/SNPEFFECT/Outputs'
+        self.path_output_root = "/Users/u0120577/SNPEFFECT/Outputs"
 
     def tearDown(self):
         self.fasta_repaired_1_A = None
 
-    @patch.object(Biopy, '_write_qblast_xml_result')
-    @patch.object(NCBIWWW, 'qblast')
-    @patch.object(Biopy, '_read_input_fasta_seq')
+    @patch.object(Biopy, "_write_qblast_xml_result")
+    @patch.object(NCBIWWW, "qblast")
+    @patch.object(Biopy, "_read_input_fasta_seq")
     def test_find_identical_blastp_hit_swissprot_for_fasta_input(self, mock__read_input_fasta_seq, mock_qblast,
                                                                  mock__write_qblast_xml_result):
+
         # arrange
-        mock__read_input_fasta_seq.return_value = '>1_A\nRVYLTFDELRETKTSEYFSLSHHPLDYRILLMDEDQDRIYVGSKDHILSLNINNIS' \
-                                                  'QEALSVFWPASTIKVEECKMAGKDPTHGCGNFVRVIQTFNRTHLYVCGSGAFSPVCTYLNRG' \
-                                                  'RRSEDQVFMIDSKCESGKGRCSFNPNVNTVSVMINEELFSGMYIDFMGTDAAIFRSLTKRNA' \
-                                                  'VRTDQHNSKWLSEPMFVDAHVIPDGTDPNDAKVYFFFKEKLTDNNRSTKQIHSMIARICPND' \
-                                                  'TGGLRSLVNKWTTFLKARLVCSVTDEDGPETHFDELEDVFLLETDNPRTTLVYGIFTTSSSV' \
-                                                  'FKGSAVCVYHLSDIQTVFNGPFAHKEGPNHQLISYQGRIPYPRPGTCPGGAFTPNMRTTKEF' \
-                                                  'PDDVVTFIRNHPLMYNSIYPIHKRPLIVRIGTDYKYTKIAVDRVNAADGRYHVLFLGTDRGT' \
-                                                  'VQKVVVLPTNNSVSGELILEELEVFKNHAPITTMKISSKKQQLYVSSNEGVSQVSLHRCHIY' \
-                                                  'GTACADCCLARDPYCAWDGHSCSRFYPTGKRRSRRQDVRHGNPLTQCR'
+        mock__read_input_fasta_seq.return_value = ">1_A\nRVYLTFDELRETKTSEYFSLSHHPLDYRILLMDEDQDRIYVGSKDHILSLNINNIS" \
+                                                  "QEALSVFWPASTIKVEECKMAGKDPTHGCGNFVRVIQTFNRTHLYVCGSGAFSPVCTYLNRG" \
+                                                  "RRSEDQVFMIDSKCESGKGRCSFNPNVNTVSVMINEELFSGMYIDFMGTDAAIFRSLTKRNA" \
+                                                  "VRTDQHNSKWLSEPMFVDAHVIPDGTDPNDAKVYFFFKEKLTDNNRSTKQIHSMIARICPND" \
+                                                  "TGGLRSLVNKWTTFLKARLVCSVTDEDGPETHFDELEDVFLLETDNPRTTLVYGIFTTSSSV" \
+                                                  "FKGSAVCVYHLSDIQTVFNGPFAHKEGPNHQLISYQGRIPYPRPGTCPGGAFTPNMRTTKEF" \
+                                                  "PDDVVTFIRNHPLMYNSIYPIHKRPLIVRIGTDYKYTKIAVDRVNAADGRYHVLFLGTDRGT" \
+                                                  "VQKVVVLPTNNSVSGELILEELEVFKNHAPITTMKISSKKQQLYVSSNEGVSQVSLHRCHIY" \
+                                                  "GTACADCCLARDPYCAWDGHSCSRFYPTGKRRSRRQDVRHGNPLTQCR"
 
-        with open('/Users/u0120577/SNPEFFECT/Outputs/test_blastp_1_A.xml') as test_1_A_xml:
+        with open("/Users/u0120577/SNPEFFECT/Outputs/test_blastp_1_A.xml") as test_1_A_xml:
             mock_qblast.return_value = test_1_A_xml
-        mock__write_qblast_xml_result.return_value = '/Users/u0120577/SNPEFFECT/Outputs/blastp_swissprot/1_A.xml'
-
-        path_fasta_input_query_seq = self.path_fasta_input_test_seq1
-        path_output_root = self.path_output_root
+        mock__write_qblast_xml_result.return_value = "/Users/u0120577/SNPEFFECT/Outputs/blastp_swissprot/1_A.xml"
 
         # action
-        result = Biopy.find_identical_blastp_hit_swissprot_for_fasta_input(path_fasta_input_query_seq,
-                                                                          path_output_root)
-        print('nothing: ' + result)
+        result = Biopy.find_identical_blastp_hit_swissprot_for_fasta_input(self.path_fasta_input_input_query_seq1,
+                                                                           self.path_output_root)
         # assert
         # self.assertEqual()
 
