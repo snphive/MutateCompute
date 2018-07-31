d86496ae1a2d8600b4fe6b4cb1c76f0e280491c0
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-07-31 18:31:27 +0200

    Rename PathForTests.TestPaths to TestPaths.TPaths. Remove word "PATH_" from enum members. Remove .value as not needed. Add paths to executables.

--- tests/TestPaths.py
+++ tests/TestPaths.py
@@ -7,52 +7,54 @@ from enum import Enum
 #
 # All paths are absolute paths unless prefix with an "REL" which indicates they are relative.
 # All paths are prefixed with "PATH"
-class PFT(Enum):
+class TPaths(Enum):
 
     # ABSOLUTE PATHS FOR TESTS ROOT
-    PATH_TESTS = "/Users/u0120577/PycharmProjects/MutateCompute/tests"
+    MC_TESTS = "/Users/u0120577/PycharmProjects/MutateCompute/tests"
 
     # CONFIGURATION FILES FROM MAIN DIRECTORY (i.e. real data)
-    PATH_CONFIG_FOR_READ_ONLY = "/Users/u0120577/PycharmProjects/MutateCompute/config"
+    CONFIG_FOR_READ_ONLY = "/Users/u0120577/PycharmProjects/MutateCompute/config"
     # INPUT FILES FROM MAIN DIRECTORY (i.e. real data)
-    PATH_INPUT_FOR_READ_ONLY = "/Users/u0120577/PycharmProjects/MutateCompute/input_data"
+    INPUT_FOR_READ_ONLY = "/Users/u0120577/PycharmProjects/MutateCompute/input_data"
 
     # The following might not even be used as entire config directory is being copied straight into /tests in one go.
-    REL_PATH_CONFIG = "/config"
-    REL_PATH_CONFIG_AGAD = "/config/agadir_config"
-    REL_PATH_CONFIG_FX = "/config/foldx_config"
-    REL_PATH_CONFIG_GLOBAL_OPTIONS = "/config/global_options"
-    REL_PATH_CONFIG_FX_ACRUNSCRIPT = "/config/foldx_config/ac_runscript"
-    REL_PATH_CONFIG_FX_BMRUNSCRIPT = "/config/foldx_config/bm_runscript"
+    REL_CONFIG = "/config"
+    REL_CONFIG_AGAD = "/config/agadir_config"
+    REL_CONFIG_JOBQ = "/config/cluster_jobq"
+    REL_CONFIG_FX = "/config/foldx_config"
+    REL_CONFIG_GLOBAL_OPTIONS = "/config/global_options"
+    REL_CONFIG_FX_ACRUNSCRIPT = "/config/foldx_config/ac_runscript"
+    REL_CONFIG_FX_BMRUNSCRIPT = "/config/foldx_config/bm_runscript"
     # ABSOLUTE PATH BUILT FROM ROOT AND RELATIVE PATHS
-    PATH_TESTS_CONFIG = PATH_TESTS.value + REL_PATH_CONFIG.value
-    PATH_TESTS_CONFIG_AGAD = PATH_TESTS.value + REL_PATH_CONFIG_AGAD.value
-    PATH_TESTS_CONFIG_FX = PATH_TESTS.value + REL_PATH_CONFIG_FX.value
-    PATH_TESTS_CONFIG_GLOBAL_OPTIONS = PATH_TESTS.value + REL_PATH_CONFIG_GLOBAL_OPTIONS.value
-    PATH_TESTS_CONFIG_FX_ACRUNSCRIPT = PATH_TESTS.value + REL_PATH_CONFIG_FX_ACRUNSCRIPT.value
-    PATH_TESTS_CONFIG_FX_BMRUNSCRIPT = PATH_TESTS.value + REL_PATH_CONFIG_FX_BMRUNSCRIPT.value
+    MC_TESTS_CONFIG = MC_TESTS + REL_CONFIG
+    MC_TESTS_CONFIG_AGAD = MC_TESTS + REL_CONFIG_AGAD
+    MC_TESTS_CONFIG_JOBQ = MC_TESTS + REL_CONFIG_JOBQ
+    MC_TESTS_CONFIG_FX = MC_TESTS + REL_CONFIG_FX
+    MC_TESTS_CONFIG_GLOBAL_OPTIONS = MC_TESTS + REL_CONFIG_GLOBAL_OPTIONS
+    MC_TESTS_CONFIG_FX_ACRUNSCRIPT = MC_TESTS + REL_CONFIG_FX_ACRUNSCRIPT
+    MC_TESTS_CONFIG_FX_BMRUNSCRIPT = MC_TESTS + REL_CONFIG_FX_BMRUNSCRIPT
 
-    # REFERENCE FILES ONLY
-    REL_PATH_REF_FILES = "/reference_files"
+    # REFERENCE FILES - NOTE: THESE ARE ONLY USED FOR TESTS
+    REL_REFFILES = "/reference_files"
     # ABSOLUTE PATH BUILT FROM ROOT AND RELATIVE PATHS
-    PATH_TESTS_REF_FILES = PATH_TESTS.value + REL_PATH_REF_FILES.value
+    MC_TESTS_REFFILES = MC_TESTS + REL_REFFILES
 
     # INPUT PATHS ONLY
     # e.g. /tests/input_data/<pdbname>/all_mutants/<fxmutantchainname>
-    REL_PATH_INPUT = "/input_data"
-    REL_PATH_ALL_MUTANTS = "/all_mutants"
+    REL_INPUT = "/input_data"
+    REL_ALL_MUTANTS = "/all_mutants"
     # ABSOLUTE PATH BUILT FROM ROOT AND RELATIVE PATHS
-    PATH_TESTS_INPUT = PATH_TESTS.value + REL_PATH_INPUT.value
+    MC_TESTS_INPUT = MC_TESTS + REL_INPUT
 
     # OUTPUT PATHS ONLY
     # e.g.#1: tests/output_data/<pdbname>/foldx/build_model/<fxmutantchainname>
     # e.g.#2: tests/output_data/<pdbname>/mutate_fasta
-    REL_PATH_OUTPUT = "/output_data"
-    REL_PATH_FX_AC = "/foldx/analyse_complex"
-    REL_PATH_FX_BM = "/foldx/build_model"
-    REL_PATH_MUTATE_FASTA = "/mutate_fasta"
+    REL_OUTPUT = "/output_data"
+    REL_FX_AC = "/foldx/analyse_complex"
+    REL_FX_BM = "/foldx/build_model"
+    REL_MUTATE_FASTA = "/mutate_fasta"
     # ABSOLUTE PATH BUILT FROM ROOT AND RELATIVE PATHS
-    PATH_TESTS_OUTPUT = PATH_TESTS.value + REL_PATH_OUTPUT.value
+    MC_TESTS_OUTPUT = MC_TESTS + REL_OUTPUT
 
     DICT_AA_1TO3 = {'A': 'ALA', 'C': 'CYS', 'D': 'ASP', 'E': 'GLU', 'F': 'PHE', 'G': 'GLY', 'H': 'HIS', 'I': 'ILE',
                     'K': 'LYS', 'L': 'LEU', 'M': 'MET', 'N': 'ASN', 'P': 'PRO', 'Q': 'GLN', 'R': 'ARG', 'S': 'SER',
@@ -62,3 +64,13 @@ class PFT(Enum):
                     'ARG': 'R', 'SER': 'S', 'THR': 'T', 'VAL': 'V', 'TRP': 'W', 'TYR': 'Y'}
     LIST_ALL_20_AA = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W',
                       'Y']
+
+    ZEUS_R_EXE = "/software/shared/apps/general/R/3.1.2/bin/Rscript"
+    ZEUS_FOLDX_EXE = "/switchlab/group/tools/FoldX_2015/FoldX"
+    ZEUS_AGADIR_EXE = "/switchlab/group/tools/agadir_10042012/agadirwrapper"
+    ZEUS_QSUB_EXE = "/opt/sge/bin/lx-amd64/"
+
+    LOCAL_R_EXE = "/usr/local/bin/R"
+    LOCAL_FOLDX_EXE = "/Users/u0120577/SNPEFFECT/executables/FoldX"
+    LOCAL_AGADIR_EXE = "/switchlab/group/tools/agadir_10042012/agadirwrapper"
+
