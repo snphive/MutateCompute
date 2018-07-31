833cde14d50a4a74342227a6d0838ce09d3c9b4d
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-07-31 18:33:23 +0200

    Replace fasta_file argument with path_fasta_file. Add path_output as an argument so caller can specify where results go.

--- src/IdentifyProtein.py
+++ src/IdentifyProtein.py
@@ -8,30 +8,30 @@ from src.Biopython import Biopy
 # mapping whatever identifier the input has with the recognised identifiers (1_A to 32431_A from RvdK's repaired pdbs)
 class IdProt(object):
 
-    DIR_BLAST_SWISPROT = 'blastp_swisprot'
+    DIR_BLASTP = 'blastp'
+    PATH_BLASTP = '/' + DIR_BLASTP
     TWO_HUNDRED_KB = 200000
-    PATH_FASTA_INPUT = 'bla bla'
-    PATH_RESULT_OUTPUT = 'bla bla'
 
-    # Manually transfer fasta files to an input folder at the path set as a constant global variable method.
+    # (Manually transfer fasta files to an input folder at the path set as a constant global variable method.)
     #
-    # fasta_file            String      FASTA input file (with .fasta extension). Filename used for new result dir.
+    #
+    # path_fasta_file       String      Absolute path to FASTA input file (with .fasta extension).
     # write_blastp_json     Boolean     True to write the blastp result dictionary to a json output file.
     # build_idmap_csv       Boolean     True to build a csv file that maps ids (accession and RvdK ids).
+    # path_output           String      Absolute path to output files. Typically output_data/<pdbname>/blastp.
     @staticmethod
-    def start(fasta_file, write_blastp_json, build_idmap_csv):
+    def start(path_fasta_file, write_blastp_json, build_idmap_csv, path_output):
+        fasta_file = path_fasta_file.split('/')[-1]
         filename = fasta_file.split('.')[0]
-        path_fasta_file = IdProt.PATH_FASTA_INPUT + '/' + filename + '.fasta'
         fasta_str = IdProt._read_fasta_file(path_fasta_file)
         blastp_result = Biopy.run_blastp(fasta_str)
-        path_blastp_xml_result = IdProt._write_blastp_xml_result(IdProt.PATH_RESULT_OUTPUT,
-                                                                 IdProt.DIR_BLAST_SWISPROT, filename, blastp_result)
+        path_blastp_xml_result = IdProt._write_blastp_xml_result(path_output, filename, blastp_result)
         blastp_result_dict = Biopy.parse_filter_blastp_xml_to_dict(path_blastp_xml_result, filename, path_fasta_file)
         if write_blastp_json:
-            IdProt._write_dict_to_json_file(IdProt.PATH_RESULT_OUTPUT, filename, blastp_result_dict)
+            IdProt._write_dict_to_json_file(path_output, filename, blastp_result_dict)
         if build_idmap_csv:
             id_map = IdProt._build_idmap(blastp_result_dict, filename)
-            IdProt._write_idmap_csv(IdProt.PATH_RESULT_OUTPUT, filename, id_map)
+            IdProt._write_idmap_csv(path_output, filename, id_map)
         return blastp_result_dict
 
     # Reads FASTA input file in and returns the text including the >name, the /n, and the amino acid sequence.
@@ -45,8 +45,8 @@ class IdProt(object):
 
     #
     @staticmethod
-    def _write_blastp_xml_result(path_output_dir, blast_dir, filename, blastp_result):
-        path_output = GUM.create_dir_tree(path_output_dir, filename, blast_dir)
+    def _write_blastp_xml_result(path_output, filename, blastp_result):
+        path_output = GUM.create_dir_tree(path_output, filename, IdProt.DIR_BLASTP)
         path_result_file = path_output + "/" + filename + ".xml"
         with open(path_result_file, 'w') as out_handle:
             out_handle.write(blastp_result.read())
@@ -62,9 +62,9 @@ class IdProt(object):
             f.write(json.dumps(blastp_result_dict))
 
     @staticmethod
-    def _build_idmap(blastp_result_dict):
+    def _build_idmap(blastp_result_dict, filename):
         #  TODO
-        # lots to do here to parse the dict or xml into a csv that can be uploaded to mysql table
+        # lots to do here to parse the dict or xmsl into a csv that can be uploaded to mysql table
         dict = {}
         return dict
 
