a667b46a0a4fa0b4160e188f5ffe0137d4b5c2cc
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-08-02 19:36:43 +0200

    Turn this script into class with methods.

--- src/Main.py
+++ src/Main.py
@@ -1,150 +1,125 @@
 import glob
-import yaml
 from src.Scheduler import Scheduler
 from src.GeneralUtilityMethods import GUM
 from src.Paths import Paths
+from src.AminoAcids import AA
+
 
 # The following 4 lines of code successfully connect to
 # import mysql.connector
 # cnx = mysql.connector.connect(user='root', password='K0yGrJ8(', host='127.0.0.1', database='mydb', port='3306')
 # cur = cnx.cursor()
 # cur.execute("CREATE TABLE testingDBConnection ( id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, title TEXT NOT NULL )")
+#
+# Currently, the entire set of programs is kicked off by calling the constructor of Main()
+# This might be done through a short script like the run_agadir.py script I wrote for the refactored Solubis code.
+# Hence run_Main.py could then be called by a cmd line or bash script "python run_Main.py".
+class Main(object):
 
+    # At the moment this is set up to run either in local or on cluster, not both. This is temporary.
+    def __init__(self, cluster):
+        globaloptions = Main._read_global_options()
+        input_pdb_list = Main._determine_which_pdbs_to_analyse(globaloptions)
+        input_fasta_list = Main._determine_which_fasta_to_analyse(globaloptions)
+        operations = Main._determine_which_operations_to_perform(globaloptions)
+        list_of_mutant_aa = Main._determine_residues_to_mutate_to()
+        path_dst_pdb_dir = Paths.SE_INPUT if cluster else Paths.MC_INPUT
+        path_src_pdb_dir = Paths.SE_PDB_REPO if cluster else Paths.LOCAL_PDB_REPO
+        GUM.copy_input_files_from_repo_to_input(path_src_pdb_dir, path_dst_pdb_dir, input_pdb_list, input_fasta_list)
+        Main._start_scheduler(operations, path_root_input, input_pdb_list, input_fasta_list, list_of_mutant_aa)
 
-mutateFasta = None
-agadir = None
-foldx = None
-scheduler = None
+    @staticmethod
+    def _read_global_options():
+        return open(Paths.MC_CONFIG_GLOBAL_OPTIONS + '/MutateCompute_Options.txt', 'r').readlines()
 
-number_of_pdbs_to_analyse = 0
-number_of_fastas_to_analyse = 0
-input_pdbs = []
-input_fastas = []
-do_mutate = False
-do_agadir = False
-do_foldx_repair = False
-do_foldx_buildmodel = False
-do_foldx_stability = False
-do_foldx_analysecomplex = False
+    @staticmethod
+    def _determine_which_pdbs_to_analyse(global_options):
+        pdb_path_list = []
+        number_of_pdbs_to_analyse = 0
+        input_pdb_list = []
+        for line in global_options:
+            if '#' in line:
+                continue
+            if 'PDBs:' in line:
+                pdb_option = line.split(':')[-1].strip(';\n').strip().lower()
+                if pdb_option == 'all'  or pdb_option == '':
+                    pdb_path_list = glob.glob(Paths.MC_INPUT + '*.pdb')
+                    for pdb_path in pdb_path_list:
+                        input_pdb_list.append(pdb_path.split('/')[-1])
+                elif pdb_option == '':
+                    number_of_pdbs_to_analyse = 0
+                elif not isinstance(pdb_option, str):
+                    number_of_pdbs_to_analyse = int(pdb_option)
+                for x in range(1, number_of_pdbs_to_analyse + 1):
+                    input_pdb_list.append(pdb_path_list[x].split('/')[-1])
+        return input_pdb_list
 
+    @staticmethod
+    def _determine_which_fasta_to_analyse(global_options):
+        fasta_path_list = []
+        number_of_fastas_to_analyse = 0
+        input_fasta_list = []
+        for line in global_options:
+            if "#" in line:
+                continue
+            if 'FASTAs:' in line:
+                fasta_option = line.split(':')[-1].strip(';\n').strip().lower()
+                if fasta_option == 'all' or fasta_option == '':
+                    fasta_path_list = glob.glob(Paths.MC_INPUT + '*.fasta')
+                    for fasta_path in fasta_path_list:
+                        input_fasta_list.append(fasta_path.split('/')[-1])
+                elif fasta_option == '':
+                    number_of_fastas_to_analyse = 0
+                elif not isinstance(fasta_option, str):
+                    number_of_fastas_to_analyse = int(fasta_option)
+                for x in range(1, number_of_fastas_to_analyse + 1):
+                    input_fasta_list.append(fasta_path_list[x].split('/')[-1])
+        return input_fasta_list
 
-with open("/Users/u0120577/PycharmProjects/MutateCompute/config/pathsAndDictionaries.yaml", 'r') as stream:
+    @staticmethod
+    def _determine_which_operations_to_perform(global_options):
+        operations = {}
+        for line in global_options:
+            if "#" in line:
+                continue
+            if "MUTATE_FASTA:" in line:
+                operations['do_mutate_fasta'] = line.split(':')[-1].strip(';\n').strip() == 'True'
+            if "AGADIR:" in line:
+                operations['do_agadir'] = line.split(':')[-1].strip(';\n').strip() == 'True'
+            if "FOLDX_REPAIR:" in line:
+                operations['do_foldx_repair'] = line.split(':')[-1].strip(';\n').strip() == 'True'
+            if "FOLDX_BUILDMODEL:" in line:
+                operations['do_foldx_buildmodel'] = line.split(':')[-1].strip(';\n').strip() == 'True'
+            if "FOLDX_STABILITY:" in line:
+                operations['do_foldx_stability'] = line.split(':')[-1].strip(';\n').strip() == 'True'
+            if "FOLDX_ANALYSECOMPLEX:" in line:
+                operations['do_foldx_analysecomplex'] = line.split(':')[-1].strip(';\n').strip() == 'True'
+        return operations
 
-    try:
+    @staticmethod
+    def _determine_residues_to_mutate_to(global_options):
+        list_of_mutant_aa = []
+        for line in global_options:
+            if '#' in line:
+                continue
+            if 'RESIDUES' in line:
+                aa_option = line.split(':')[-1].strip(';\n').strip()
+                if aa_option == 'all' or aa_option == '':
+                    list_of_mutant_aa = AA.LIST_ALL_20_AA
+                else:
+                    for aa in aa_option:
+                        list_of_mutant_aa.append(aa)
+        return list_of_mutant_aa
 
-        paths_and_dictionaries = yaml.load(stream)
-        path_zeus_R_exe = paths_and_dictionaries['ROOT']['path_zeus_R_exe']
-        path_zeus_FoldX_exe = paths_and_dictionaries['ROOT']['path_zeus_FoldX_exe']
-        path_zeus_Agadir_exe = paths_and_dictionaries['ROOT']['path_zeus_Agadir_exe']
-        path_zeus_Qsub_exe = paths_and_dictionaries['ROOT']['path_zeus_Qsub_exe']
-        path_local_R_exe = paths_and_dictionaries['ROOT']['path_local_R_exe']
-        path_local_FoldX_exe = paths_and_dictionaries['ROOT']['path_local_FoldX_exe']
-        path_local_Agadir_exe = paths_and_dictionaries['ROOT']['path_local_Agadir_exe']
+    @staticmethod
+    def _start_scheduler(operations, path_input, input_pdb_list, input_fasta_list, list_of_mutant_aa):
+        if operations['do_mutate_fasta'] or operations['do_agadir'] or operations['do_foldx_repair'] \
+                or operations['do_foldx_buildmodel'] or operations['do_foldx_stability'] \
+                or operations['do_foldx_analysecomplex']:
+            Scheduler.start(operations, path_input, input_pdb_list, input_fasta_list, list_of_mutant_aa)
+        else:
+            print("All of the operations are set to FALSE in MutateCompute_Options.txt file - hence nothing to do")
 
-        path_zeus_SnpEffect = paths_and_dictionaries['ROOT']['path_zeus_SnpEffect']
-        path_local_MutateCompute = paths_and_dictionaries['ROOT']['path_local_MutateCompute']
-
-        path_rel_src = paths_and_dictionaries['ROOT']['path_rel_src']
-        path_rel_Inputs_PDBs = paths_and_dictionaries['ROOT']['path_rel_Inputs_PDBs']
-        path_rel_Inputs_Fasta = paths_and_dictionaries['ROOT']['path_rel_Inputs_Fasta']
-        path_rel_Inputs_Options = paths_and_dictionaries['ROOT']['path_rel_Inputs_Options']
-        path_rel_Outputs_PDBs = paths_and_dictionaries['ROOT']['path_rel_Outputs_PDBs']
-
-        path_rel = paths_and_dictionaries['ROOT']['path_rel']
-        path_rel_Cluster = paths_and_dictionaries['ROOT']['path_rel_Cluster']
-        path_rel_Agadir = paths_and_dictionaries['ROOT']['path_rel_Agadir']
-        path_rel_FoldX = paths_and_dictionaries['ROOT']['path_rel_FoldX']
-        path_rel_FoldX_BuildModel = paths_and_dictionaries['ROOT']['path_rel_FoldX_BuildModel']
-        path_rel_FoldX_Repair = paths_and_dictionaries['ROOT']['path_rel_FoldX_Repair']
-        path_rel_FoldX_AnalyseComplex = paths_and_dictionaries['ROOT']['path_rel_FoldX_AnalyseComplex']
-
-        dict_aa_1to3 = paths_and_dictionaries['ROOT']['dict_aa_1to3']
-        dict_aa_3to1 = paths_and_dictionaries['ROOT']['dict_aa_3to1']
-        list_all_20_aa = paths_and_dictionaries['ROOT']['list_all_20_aa']
-
-    except yaml.YAMLError as exc:
-        print(exc)
-
-mutate_compute_options_file = open('../options/MutateCompute_Options.txt', 'r').readlines()
-for line in mutate_compute_options_file:
-    if '#' in line:
-        continue
-    if 'PDBs:' in line:
-        pdb_option = line.split(':')[-1].strip(';\n').strip()
-        if pdb_option == 'All':
-            pdb_paths = glob.glob(Paths.PATH_LOCAL_MUTATECOMPUTE + Paths.PATH_REL_INPUTS_PDBS + '*.pdb')
-            for pdb_path in pdb_paths:
-                input_pdbs.append(pdb_path.split('/')[-1])
-        elif pdb_option == '':
-            number_of_pdbs_to_analyse = 0
-        elif not isinstance(pdb_option, str):
-            number_of_pdbs_to_analyse = int(pdb_option)
-        for x in range(1, number_of_pdbs_to_analyse + 1):
-            input_pdbs.append(pdb_paths[x].split('/')[-1])
-    print('PDBs to analyse:\t\t' + ",\t".join(input_pdbs))
-
-    if 'FASTAs:' in line:
-        fasta_option = line.split(':')[-1].strip(';\n').strip()
-        if fasta_option == 'All':
-            fasta_paths = glob.glob(path_local_MutateCompute + path_rel_Inputs_Fasta + '*.fasta')
-            for fasta_path in fasta_paths:
-                input_fastas.append(fasta_path.split('/')[-1])
-        elif fasta_option == '':
-            number_of_fastas_to_analyse = 0
-        elif not isinstance(fasta_option, str):
-            number_of_fastas_to_analyse = int(fasta_option)
-        for x in range(1, number_of_fastas_to_analyse + 1):
-            input_fastas.append(fasta_paths[x].split('/')[-1])
-    print('FASTA files to analyse:\t\t' + ",\t".join(input_fastas))
-
-    if 'MUTATE_FASTA:' in line:
-        do_mutate_fasta = line.split(':')[-1].strip(';\n').strip()
-    if 'AGADIR:' in line:
-        do_agadir = line.split(':')[-1].strip(';\n').strip()
-    if 'FOLDX_REPAIR:' in line:
-        do_foldx_repair = line.split(':')[-1].strip(';\n').strip()
-    if 'FOLDX_BUILDMODEL:' in line:
-        do_foldx_buildmodel = line.split(':')[-1].strip(';\n').strip()
-    if 'FOLDX_STABILITY:' in line:
-        do_foldx_stability = line.split(':')[-1].strip(';\n').strip()
-    if 'FOLDX_ANALYSECOMPLEX:' in line:
-        do_foldx_analysecomplex = line.split(':')[-1].strip(';\n').strip()
-
-operations = {
-    'do_mutate_fasta': do_mutate_fasta,
-    'do_agadir': do_agadir,
-    'do_foldx_repair': do_foldx_repair,
-    'do_foldx_buildmodel': do_foldx_buildmodel,
-    'do_foldx_stability': do_foldx_stability,
-    'do_foldx_analysecomplex': do_foldx_analysecomplex,
-}
-
-
-def _build_dir_trees():
-    build_local_dir_tree = 'True'
-    build_zeus_dir_tree = 'False'
-
-    rel_path_list = [GUM.path_rel_src, GUM.path_rel_Inputs_PDBs, GUM.path_rel_Inputs_Fasta,
-                     GUM.path_rel_Inputs_Options, GUM.path_rel_Outputs_PDBs]
-
-    if not build_local_dir_tree and not build_zeus_dir_tree:
-        raise ValueError('Both options false - neither local or cluster trees will be created')
-
-    else:
-
-        if build_local_dir_tree:
-            GUM.create_dir_tree_one_level(GUM.path_local_MutateCompute, rel_path_list)
-
-        if build_zeus_dir_tree:
-            GUM.create_dir_tree_one_level(GUM.path_zeus_SnpEffect, rel_path_list)
-
-
-if do_mutate_fasta or do_agadir or do_foldx_repair or do_foldx_buildmodel or do_foldx_stability or do_foldx_analysecomplex:
-    _build_dir_trees()
-    scheduler = Scheduler(operations, input_pdbs, input_fastas, list_all_20_aa)
-    scheduler.start()
-else:
-    print('All of the operations are set to FALSE in MutateCompute_Options.txt file - hence nothing to do')
 
 
 # cnx is the mysql connector (see top of script)
