5a769dd22b7a7799020183a11409b46a5695da6a
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-08-17 11:27:45 +0200

    Update & improve consistency of names and complete documentation.

--- src/IdentifyProtein.py
+++ src/IdentifyProtein.py
@@ -1,6 +1,5 @@
 import os
 import json
-from src.GeneralUtilityMethods import GUM
 from src.Biopython import Biopy
 from src.Paths import Paths
 
@@ -9,17 +8,20 @@ from src.Paths import Paths
 # mapping whatever identifier the input has with the recognised identifiers (1_A to 32431_A from RvdK's repaired pdbs)
 class IdProt(object):
 
-    TWO_HUNDRED_KB = 200000
-
-    # (Manually transfer fasta files to an input folder at the path set as a constant global variable method.)
+    # (This method relies on the presence of fastafiles in the specified folder, in order for them to be run on Blastp,
+    # This transfer is currently done manually)
     #
     # path_fastafile            String      Absolute path to FASTA input file (with .fasta extension).
-    # write_blastp_json         Boolean     True to write the blastp result dictionary to a json output file.
     # path_output               String      Absolute path to output files. Typically output_data/<pdbname>/blastp.
-    # write_idmaps_for_mysqldb  Boolean     True to build & write csv/xml files that map ids (accession and RvdK ids).
+    # write_idmaps_for_mysqldb  Boolean     True to build dict mapping RvdK ids to swsprt acc & write files.
+    # write_csv                 Boolean     True to write csvfiles.
+    # write_xml                 Boolean     True to write xmlfiles.
+    # write_json                Boolean     True to write jsonfiles.
+    #
+    # Returns the dictionary data structure representation of the parsed & filtered Blastp run result.
     @staticmethod
-    def map_seq_to_swsprt_acc_id_and_write_files(path_fastafile, path_output, write_idmaps_for_mysldb, make_csv=True,
-                                                 make_xml=True, make_json=False):
+    def map_seq_to_swsprt_acc_id_and_write_files(path_fastafile, path_output, write_idmaps_for_mysldb, write_csv=True,
+                                                 write_xml=True, write_json=False):
         with open(path_fastafile) as fasta_io:
             fasta_str = fasta_io.read()
         blastp_result = Biopy.run_blastp(fasta_str)
@@ -27,15 +29,15 @@ class IdProt(object):
         path_blstp_xml = IdProt._write_blast_xml(path_output, fastafile_name, blastp_result)
         blastp_dict = Biopy.parse_and_filter_blastp_xml_to_dict(path_blstp_xml, fastafile_name, path_fastafile)
         if write_idmaps_for_mysldb:
-            IdProt._write_idmaps_for_mysqldb(path_output, blastp_dict, make_csv=make_csv, make_xml=make_xml,
-                                             make_json=make_json)
+            IdProt._write_idmaps_for_mysqldb(path_output, blastp_dict, write_csv=write_csv, write_xml=write_xml,
+                                             write_json=write_json)
         return blastp_dict
 
     # Writes the blastp result file to an xml file in a output_data subdir called blastp.
     #
-    # path_output       String              Abs path to output root dir, typically /output_data
+    # path_output       String              Absolute path to output root dir, typically /output_data.
     # fastafile_name    String              Used for naming the xml file.
-    # blastp_result     io.TextIOWrapper    Biopython's blastp output. (Is a buffered text stream).
+    # blastp_result     io.TextIOWrapper    Biopython's raw blastp output. (Is a buffered text stream).
     #
     # Returns path to the newly-written xml file.
     @staticmethod
@@ -46,41 +48,48 @@ class IdProt(object):
             out_handle.write(blastp_result.read())
             blastp_result.close()
 
-        if os.stat(path_output_blastp_xmlfile).st_size > IdProt.TWO_HUNDRED_KB:
+        if os.stat(path_output_blastp_xmlfile).st_size > IdProt.Strng.EST_MAX_SZE_BLSTP_RUN_200_KB:
             raise ValueError("blast xml output size is over 200 KB in size. Something may have gone wrong with the "
                              "blastp run.")
 
         return path_output_blastp_xmlfile
 
+    # Write to file the result of blastp run (that has already been parsed & filtered from the raw blastp result).
+    #
+    # path_output   String          Absolute path to the output-data dir.
+    # blastp_dict   Dictionary      Data structure parsed and filtered from Blastp run.
+    # write_xml     Boolean         True to write xmlfile represetation of blastp_dict, True by default.
+    # write_csv     Boolean         True to write csvfile represetation of blastp_dict, True by default.
+    # write_json    Boolean         True to write jsonfile represetation of blastp_dict, False by default.
     @staticmethod
-    def _write_idmaps_for_mysqldb(path_output, blastp_dict, make_xml=True, make_csv=True, make_json=False):
+    def _write_idmaps_for_mysqldb(path_output, blastp_dict, write_xml=True, write_csv=True, write_json=False):
         idmap = {IdProt.Strng.SEQ_ID.value: blastp_dict[IdProt.Strng.QRY_SEQ_ID.value]}
-        fullname_search_str = IdProt.Strng.NAME_SRCH_STR.value
+        name_search_str = IdProt.Strng.NAME_SRCH_STR.value
         altname_search_str = IdProt.Strng.ALTNAME_SRCH_STR.value
         flags_search_str = IdProt.Strng.FLGS_SRCH_STR.value
         for alignment in blastp_dict[IdProt.Strng.IDENT_ALIGN_LST.value]:
             idmap[IdProt.Strng.ACC_NUM.value] = alignment[IdProt.Strng.ACC_NUM.value]
             idmap[IdProt.Strng.LEN.value] = alignment[IdProt.Strng.LEN.value]
-            full_alt_names_flags = IdProt._extract_names_flags(alignment[IdProt.Strng.HIT_DEF.value],
-                                                            fullname_search_str, altname_search_str, flags_search_str)
-            idmap[IdProt.Strng.NAME.value] = full_alt_names_flags[fullname_search_str]
+            full_alt_names_flags = IdProt._extract_names_flags(alignment[IdProt.Strng.HIT_DEF.value], name_search_str,
+                                                               altname_search_str, flags_search_str)
+            idmap[IdProt.Strng.NAME.value] = full_alt_names_flags[name_search_str]
             idmap[IdProt.Strng.ALTNAME.value] = full_alt_names_flags[altname_search_str]
             idmap[IdProt.Strng.FLGS.value] = full_alt_names_flags[flags_search_str]
             idmap[IdProt.Strng.STRT_POS.value] = alignment[IdProt.Strng.HSP_DICT.value][IdProt.Strng.SBJCT_STRT.value]
             idmap[IdProt.Strng.END_POS.value] = alignment[IdProt.Strng.HSP_DICT.value][IdProt.Strng.SBJCT_END.value]
-        if make_csv:
+        if write_csv:
             IdProt._write_idmap_csv(path_output, idmap)
-        if make_xml:
+        if write_xml:
             IdProt._write_idmap_xml(path_output, idmap)
-        if make_json:
+        if write_json:
             IdProt._write_idmap_jsonfile(path_output, idmap)
 
     # Converts data passed in a dictionary data structure to csv format and writes it to the blastp directory.
     #
-    # path_output   String          Absolute path to the output root directory for the destination dir of the csv file.
+    # path_output   String          Abs path of output_data dir, destination for xml file.
     # idmap         Dictionary      Data structure of values taken from blastp run that are parsed to a csv format.
     #
-    # Returns the xml elements (with one attribute value) and newlines in their own element in a list.
+    # Returns the xml elements (with one attribute value for id) and newlines in their own element in a list.
     @staticmethod
     def _write_idmap_xml(path_output, idmap):
         xml_list = [IdProt.Strng.XML_PROLOG.value, '\n']
@@ -102,7 +111,7 @@ class IdProt(object):
 
     # Converts data passed in a dictionary data structure to csv format and writes it to the blastp directory.
     #
-    # path_output   String          Absolute path to the output root directory for the destination dir of the csv file.
+    # path_output   String          Abs path of output_data dir, destination for csv file.
     # idmap         Dictionary      Data structure of values taken from blastp run that are parsed to a csv format.
     #
     # Returns the values and commas and newlines in their own element in a list.
@@ -126,7 +135,10 @@ class IdProt(object):
             idmap_file.write(''.join(csv_str))
         return csv_list
 
-    # Reduces code duplication. Despite very simple code, it is made slightly more verbose with the need for a try
+    # Writes out a json file of the idmap (dictionary) using an imported library method from 'json' module.
+    #
+    # path_output   String      Abs path of output_data dir, destination for json file.
+    # idmap         Dictionary  Data structure representation of parsed & filtered Blastp runnresult.
     @staticmethod
     def _write_idmap_jsonfile(path_output, idmap):
         path_output_blastp_fastaname = IdProt._build_output_blastp_dir(path_output, idmap[IdProt.Strng.SEQ_ID.value])
@@ -134,7 +146,11 @@ class IdProt(object):
         with open(path_output_blastp_fastaname_jsonfile, 'w') as f:
             f.write(json.dumps(idmap))
 
-    # except clause, making its encapsulation for reuse worthwhile.
+    # To make directory and all intermediate directories, encapsulated in try/except clause as pre-existing result in
+    # exceptions being thrown.
+    #
+    # path_output   String      Abs path of the output_data root directory of tree to be built here.
+    # fastadirname  String      Name of fastafile (without extension)
     @staticmethod
     def _build_output_blastp_dir(path_output, fastadirname):
         path_output_blastp_fastadirname = os.path.join(path_output, Paths.DIR_BLASTP.value, fastadirname +
@@ -148,15 +164,19 @@ class IdProt(object):
     # NOTE: This method assumes that the 'Recname', 'Altname', 'Flags' are always written in this order. It also
     # assumes that the Flag is the last string of any form in the line.
     # I don't know yet if these assumptions are correct.
+    #
+    # hit_def           String      A value from Blastp result, typically includes semi-colon separated text regarding
+    #                               names and flags.
+    # *search_strngs    List        Search strings, typically name, altname, flags.
     @staticmethod
-    def _extract_names_flags(hit_def, *names_flag):
+    def _extract_names_flags(hit_def, *search_strngs):
         full_alt_names_flag = {}
-        for search_str in names_flag:
+        for search_str in search_strngs:
             try:
                 start_index = hit_def.index(search_str) + len(search_str)
                 if search_str == IdProt.Strng.FLGS_SRCH_STR.value:
                     full_alt_names_flag[search_str] = hit_def[start_index:]
-                    IdProt.__is_reasonable_length(len(full_alt_names_flag[search_str]))
+                    IdProt.__is_reasonable_length(full_alt_names_flag[search_str])
                     break
                 for i, char in enumerate(hit_def[start_index:], start=start_index):
                     if char == ';':
@@ -168,16 +188,21 @@ class IdProt(object):
                 print('Did not find ' + search_str + ' in this hit_def: ' + hit_def)
         return full_alt_names_flag
 
+    # name_or_flag      String      Text to be tested for length
+    #
+    # Returns True if text passed is between 2 and 30 characters long. I cannot say for sure that there are no names
+    # or flags that are 1 character long or more than 30 characters long - this is running assumption.
     @staticmethod
-    def __is_reasonable_length(name_or_flag_length):
+    def __is_reasonable_length(name_or_flag):
         reasonable_max_name_or_flag_len = 30
-        if name_or_flag_length == 0 | name_or_flag_length > reasonable_max_name_or_flag_len:
+        if len(name_or_flag) < 2 | len(name_or_flag) > reasonable_max_name_or_flag_len:
             raise ValueError('Full name, altname or flag is either not there at all or seems too long.')
         return True
 
     from enum import Enum
 
     class Strng(Enum):
+        EST_MAX_SZE_BLSTP_RUN_200_KB = 200000
         SEQ_ID = 'sequence_id'
         QRY_SEQ_ID = 'query_seq_id'
         NAME_SRCH_STR = 'RecName: Full='
