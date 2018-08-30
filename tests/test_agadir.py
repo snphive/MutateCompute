4bca6991673feadca3e0a97acc7949d092bb35c1
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-08-30 12:13:20 +0200

    Debug/update test method.

--- tests/test_agadir.py
+++ tests/test_agadir.py
@@ -9,34 +9,34 @@ import glob
 class TestAgadir(TestCase):
 
     def setUp(self):
-        self.agadir = Agadir()
+        self.agadir = Agadir(use_cluster=False, use_multithread=False)
 
     def tearDown(self):
         self.agadir = None
 
-    def test_copy_files_from_output_fastafilemutants_dir_to_input(self):
+    def test_copy_files_output_to_input_fastasubdirs(self):
         # arrange
-        # path_repo_fastafiles = TPLS.REPO_PDB_FASTA.value + '/**/*.fasta'
         path_repo_fastafilelist = glob.glob(TPLS.REPO_PDB_FASTA.value + '/fastas_10/*.fasta')
-
-        path_dst_arrange = os.path.join(TPLS.MC_TESTS_OUTPUT.value, TPLS.DIR_FASTAS.value)
-        GUM.linux_copy_files_into_own_subdir(path_src_filelist=path_repo_fastafilelist, path_dst=path_dst_arrange)
-        # path_dst_arrange = os.path.join(path_dst_arrange, TPLS.DIR_MUTANTS.value)
-        expected_filelist = ['1_A.fasta', '1_B.fasta', '2_A.fasta', '3_A.fasta', '3_B.fasta',
-                             '4_A.fasta', '5_A.fasta', '5_B.fasta', '5_C.fasta', '5_D.fasta']
-        path_dst_input = os.path.join(TPLS.MC_TESTS_INPUT.value, TPLS.DIR_FASTAS.value)
+        path_output_fastas = os.path.join(TPLS.MC_TESTS_OUTPUT.value, TPLS.DIR_FASTAS.value)
+        GUM.linux_copy_files(path_repo_fastafilelist, path_output_fastas, into_own_subdirs=True)
+        expected_filelist = ['1_A.fasta', '1_B.fasta', '2_A.fasta', '3_A.fasta', '3_B.fasta', '4_A.fasta', '5_A.fasta',
+                             '5_B.fasta', '5_C.fasta', '5_D.fasta']
+        path_input_fastas = os.path.join(TPLS.MC_TESTS_INPUT.value, TPLS.DIR_FASTAS.value)
+        expected_path_filelist = [path_input_fastas + '/' + file for file in expected_filelist]
+        GUM.linux_remove_files_in_dir(path_input_fastas)
         # act
-        filelist_rtn = self.agadir.copy_files_from_output_to_input_subdirs(path_output=path_dst_arrange,
-                                                                           path_input=path_dst_input)
-        actual_filelist = os.listdir(path_dst_input)
+        self.agadir.copy_files_output_to_input_fastasubdirs(path_output_fastas, path_input_fastas, filename_list=None,
+                                                            into_own_subdirs=True)
+        actual_path_filelist = glob.glob(path_input_fastas + '/*.fasta')
         # assert
-        self.assertListEqual(expected_filelist, actual_filelist)
-        self.assertListEqual(expected_filelist, filelist_rtn)
+        self.maxDiff = None
+        self.assertListEqual(expected_path_filelist, actual_path_filelist)
 
     def test_compute(self):
         # arrange
+
         # act
+        self.agadir.compute(path_input_fastas=TPLS.MC_TESTS_INPUT_FASTAS.value, path_output=TPLS.MC_TESTS_OUTPUT.value)
         # assert
-        self.assertTrue()
-        self.assertEqual()
+
 
