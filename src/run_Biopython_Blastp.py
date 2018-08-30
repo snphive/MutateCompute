af100bdfab19e731e51dc2f92ab6c645329c5d00
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-08-30 19:43:27 +0200

    Try to get return value from the biopython blast run on cluster.

--- src/run_Biopython_Blastp.py
+++ src/run_Biopython_Blastp.py
@@ -1,4 +1,9 @@
 from src.Biopython import Biopy
 import sys
 
-Biopy.run_blastp(fasta_str=sys.argv[1])
+
+# fasta_str = fasta_str=sys.argv[1]
+
+
+def run_blastp(fasta_str=sys.argv[1]):
+    return Biopy.run_blastp(fasta_str)
