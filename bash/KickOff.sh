bf18363be6b5d9c8f9128f23ddcd158e896679cb
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-08-30 12:12:26 +0200

    Add argument to indicate cluster use or not.

--- bash/KickOff.sh
+++ bash/KickOff.sh
@@ -3,5 +3,5 @@ source /Users/u0120577/.profile
 source /Users/u0120577/PycharmProjects/MutateCompute/venv/bin/activate
 
 #!/usr/bin/env python3
-python3 ./KickOff.py
+python3 ./KickOff.py 'use_cluster=False'
 
