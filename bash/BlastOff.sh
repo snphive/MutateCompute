9693d4ae862e96d622ca394945ab78b6680675b4
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-08-30 11:58:59 +0200

    Add path to blast.py adn argument

--- bash/BlastOff.sh
+++ bash/BlastOff.sh
@@ -3,4 +3,4 @@ source /Users/u0120577/.profile
 source /Users/u0120577/PycharmProjects/MutateCompute/venv/bin/activate
 
 #!/usr/bin/env python3
-python3 ./BlastOff.py
+python3 /Users/u0120577/PycharmProjects/MutateCompute/src/BlastOff.py 'use_cluster=False'
