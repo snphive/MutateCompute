a7960c38ceb9bfbbb0d3f6d0ec95d43926169374
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-09-12 10:54:39 +0200

    Converting improvised documentation (javadoc-like) to pycharm-supported docstring.

--- src/Main.py
+++ src/Main.py
@@ -21,34 +21,34 @@ import natsort
 # 3. Executes KickOff.py via python3 command to indicate that version 3 and not 2 should be used here.
 class Main(object):
 
-    # All algorithmic analyses start from here.
-    # IdentifyProtein.py which runs Blast analyses is the only program that currently runs independently.
-    #
-    # use_cluster                   Boolean     True if program(s) should run on the cluster (whereupon paths are set
-    #                                           to zeus paths). Currently set to false by default.
-    # operations                    Dict        Operations flagged True to run, False to not run.
-    # use_multithread               Boolean     True if program(s) should be spawned on separate threads/processes.
-    # path_input                    String      Abs path to input root dir
-    # path_output                   String      Abs path to output root dir
-    # path_wanted_pdbfile_list      String      Abs path to repo of pdbfiles.
-    # path_wanted_fastafile_list    String      Abs path to repo of fastafiles.
-    # mutant_aa_list                List        All amino acids that mutation operations will mutate residues to.
-    #
-    # At the moment, this is set up to run either in local or on cluster, not both.
-    def __init__(self, use_cluster: bool, operations: dict, use_multithread, path_input, path_output, path_wanted_pdbfile_list,
-                 path_wanted_fastafile_list, mutant_aa_list):
+    def __init__(self, operations: dict, use_multithread: bool, path_input: str, path_output: str, path_pdbfiles: list,
+                 path_fastafiles: list, amino_acids: list):
+        """
+        All algorithmic analyses start from here. IdentifyProtein.py which runs Blast analyses is the only program that
+        currently runs independently.
+        :param operations: Operations flagged True to run, False to not run.
+        :param use_multithread: True if program(s) should be spawned on separate threads/processes.
+        :param path_input: Abs path to input root dir
+        :param path_output: Abs path to output root dir
+        :param path_pdbfiles: Abs path to repo of pdbfiles.
+        :param path_fastafiles: Abs path to repo of fastafiles.
+        :param amino_acids: All amino acids that mutation operations will mutate residues to.
+        """
         if operations == {} or Main._all_ops_are_false(operations):
             raise ValueError("All options in 'operations' were either set to FALSE or some typographical error. "
                              "Check /configuration/global_options/global_options.txt was written correctly")
         elif operations['do_mutate_fasta'] or operations['do_agadir'] or operations['do_foldx_repair'] \
                 or operations['do_foldx_buildmodel'] or operations['do_foldx_stability'] \
                 or operations['do_foldx_analysecomplex']:
-            Scheduler.start(use_cluster, operations, use_multithread, path_input, path_output,
-                            path_pdbfile_list=path_wanted_pdbfile_list, path_fastafile_list=path_wanted_fastafile_list,
-                            mutant_aa_list=mutant_aa_list, write_1_fasta_only=True, write_fasta_per_mut=False)
+            Scheduler.start(operations, use_multithread, path_input, path_output, path_pdbfiles,
+                            path_fastafiles, amino_acids, write_1_fasta_only=True, write_fasta_per_mut=False)
 
     @staticmethod
-    def _all_ops_are_false(operations):
+    def _all_ops_are_false(operations: dict):
+        """
+        :param operations:
+        :return: True indicates all oprations are set to False in global_options.txt.
+        """
         if not operations['do_mutate_fasta'] \
                 and not operations['do_agadir'] \
                 and not operations['do_foldx_repair'] \
@@ -59,39 +59,39 @@ class Main(object):
         else:
             return False
 
-    # Reads the global_options.txt in /configuration/global_options breaking the text up according to newlines.
-    #
-    # path_globaloptions_file   String      Abs path to global_options file (incl. extension).
-    #
-    # Returns the text of the file as a list of strings for each line ending with \n.
     @staticmethod
-    def _read_global_options(path_globaloptions_file):
+    def _read_global_options(path_globaloptions_file: str):
+        """
+        Reads the global_options.txt in /configuration/global_options breaking the text up according to newlines.
+        :param path_globaloptions_file: Abs path to global_options file (incl. extension).
+        :return: Text of the file as a list of strings for each line ending with \n.
+        """
         with open(path_globaloptions_file, 'r') as globaloptions:
             globaloptions_lines = globaloptions.readlines()
         return globaloptions_lines
 
-    # Takes the /configuration/global_options/global_options.txt text as a list of \n separated lines.
-    #
-    # Reads the option given at either "PDBs" and "FASTAs".
-    # The option is expected to be in one of four forms: "", "all", "<filename(s)>", or "<number>".
-    # '' indictes no files - returns None.
-    # 'all' indicates all files in specified input source directory.
-    # '<filename(s)>' indicates specific files by name (if found in the specified input source directory).
-    # '<number>' indicates the maximum number of files from the naturally-sorted list that is based on what files are
-    # present in the specified input_data source directory.
-    # E.g. line = "PDBs: 4;\n", returns ['RepairPDB_1.pdb', 'RepairPDB_2.pdb', 'RepairPDB_3.pdb', 'RepairPDB_4.pdb'].
+
     #
     # Note the risk of inefficiency where the program spends a lot of time sorting the source directory to build the
     # list of available target files, but only a relatively small number of files will be used E.g. source dir sorts
     # about 30,000 pdbs for user to get access to first 5 only!
-    #
-    # globaloptions_lines   List        Alphanumeric text lines of the global options ending with "\n".
-    # path_repo             String      Path to the repository files (including pdbfiles and fastafiles).
-    #
-    # Returns a list of pdbfiles or fastafiles according to global_options.txt.
-    # The list can only include files that are actually in the path_repo (typically a repository dir)
     @staticmethod
-    def _build_filelist_for_analysis(globaloptions_lines, path_repo_pdbs_or_fastas):
+    def _build_filelist_for_analysis(globaloptions_lines: list, path_repo_pdbs_or_fastas: str):
+        """
+        Takes the /configuration/global_options/global_options.txt text as a list of \n separated lines.
+        Reads the option given at either "PDBs" and "FASTAs".
+        The option is expected to be in one of four forms: "", "all", "<filename(s)>", or "<number>".
+        '' indictes no files - returns None.
+        'all' indicates all files in specified input source directory.
+        '<filename(s)>' indicates specific files by name (if found in the specified input source directory).
+        '<number>' indicates the maximum number of files from the naturally-sorted list that is based on what files are
+        present in the specified input_data source directory.
+        E.g. line = "PDBs: 4;\n", returns ['RepairPDB_1.pdb', 'RepairPDB_2.pdb', 'RepairPDB_3.pdb', 'RepairPDB_4.pdb'].
+        :param globaloptions_lines: Alphanumeric text lines of the global options ending with "\n".
+        :param path_repo_pdbs_or_fastas: Path to the repository files (including pdbfiles and fastafiles).
+        :return: list of pdbfiles or fastafiles according to global_options.txt. The list can only include files that
+        are actually in the path_repo (typically referred to here as a repository dir)
+        """
         file_list = []
         file_extension = Str.PDBEXT.value
         pdbs_or_fastas_option = 'PDBs'
@@ -132,15 +132,15 @@ class Main(object):
                 break
         return file_list
 
-    # Determines which operations to perform, according to the global options file - True or False indicates to perform
-    # this operation or not, respectively.
-    # Format expected is "True" or "False" comes after ":" and before ";\n"
-    #
-    # globaloptions_lines   List    Alphanumeric text lines of the global options ending with "\n".
-    #
-    # Returns dictionary. Key is operation text, Value is boolean.
     @staticmethod
-    def _determine_which_operations_to_perform(globaloptions_lines):
+    def _determine_which_operations_to_perform(globaloptions_lines: list):
+        """
+        Determines which operations to perform, according to the global options file - True or False indicates to
+        perform this operation or not, respectively. Format expected is "True" or "False" comes after ":" and before
+        ";\n"
+        :param globaloptions_lines: Alphanumeric text lines of the global options ending with "\n".
+        :return: dict with operation str as key, True/False boolean as value.
+        """
         operations = {}
         for line in globaloptions_lines:
             if "#" in line:
@@ -159,14 +159,14 @@ class Main(object):
                 operations['do_foldx_analysecomplex'] = Main.__is_true(line)
         return operations
 
-    # Determines which residues to mutate to according to the global_options file.
-    # Given either a list of residues in FASTA) format, or the word "All" to indicate all 20 residues.
-    #
-    # globaloptions_lines   List    Alphanumeric text lines of the global options ending with "\n".
-    #
-    # Returns list of amino acids that will be used to mutate protein sequence to (at every position).
     @staticmethod
-    def _determine_residues_to_mutate_to(globaloptions_lines):
+    def _determine_residues_to_mutate_to(globaloptions_lines: list):
+        """
+        Determines which residues to mutate to according to the global_options file. Given either a list of residues
+        in FASTA) format, or the word "All" to indicate all 20 residues.
+        :param globaloptions_lines: Alphanumeric text lines of the global options ending with "\n".
+        :return: list of amino acids that will be used to mutate protein sequence to (at every position).
+        """
         mutant_aa_list = []
         for line in globaloptions_lines:
             if '#' in line:
@@ -181,20 +181,20 @@ class Main(object):
                             mutant_aa_list.append(aa)
         return mutant_aa_list
 
-    # line   String    Alphanumeric text line of the global options ending with "\n".
-    #                  Must have format "option name:<option value>;\n"
-    #
-    # Returns Boolean True if text is "True", False if text is "False".
     @staticmethod
-    def __is_true(line):
+    def __is_true(line: str):
+        """
+        :param line: Alphanumeric text of global options ending with "\n". Expecting "option name:<option value>;\n"
+        :return: True boolean if text string is "True", False if text is "False".
+        """
         return Main.__get_text_after_colon_before_semi(line) == 'TRUE'
 
-    # line   String    Alphanumeric text line of the global options ending with "\n".
-    #                  Must have format "option name:<option value>;\n"
-    #
-    # Returns all text string after a colon and before a semi-colon, stripping out any white space.
     @staticmethod
-    def __get_text_after_colon_before_semi(line):
+    def __get_text_after_colon_before_semi(line: str):
+        """
+        :param line: Alphanumeric text of global options ending with "\n". Expecting "option name:<option value>;\n"
+        :return: all text string after a colon and before a semi-colon, stripping out any white space.
+        """
         return line.split(':')[-1].strip(';\n').strip()
 
     # Validates the "PDBs:" or "FASTAs:" has expected values: "", "all", "filename", "<number within range 1 to 1000>"
