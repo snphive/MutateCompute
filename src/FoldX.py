7d544adae54b594435c662f8b6b924b674f67c5a
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-09-13 18:16:23 +0200

    Write foldx buildmodel method.

--- src/FoldX.py
+++ src/FoldX.py
@@ -1,8 +1,9 @@
 import subprocess
 import os
+from src.Str import Str
+from src.Paths import Paths
 from src.GeneralUtilityMethods import GUM
 from src.Cluster import Cluster
-from src.Paths import Paths
 
 
 class FoldX(object):
@@ -20,57 +21,34 @@ class FoldX(object):
 
     class BuildModel(object):
 
-        FXBM_jobname_prefix = 'FXBM_'
-
         def __init__(self):
             print('buildmodel constructor')
 
         # Mutate specified amino acids in this pdb to all listed amino acids using FoldX BuildModel,
         # FoldX uses a runscript file, which must be written here.
         #
-        # pdb                   String   Input pdb to be mutated.
-        # mutate_to_aa_list     List     List of amino acids that you want to mutate your pdb to.
+        # path_pdbfile                   String   Input pdb to be mutated.
+        # mutant_aa_list        List     List of amino acids that you want to mutate your pdb to.
         # write_wt_fastafiles   Boolean  True/False is you want to write the wild-type sequence of the input pdb out.
-        def mutate_residues_of_pdb(self, pdb, mutate_to_aa_list, write_wt_fastafiles):
-            pdbname = pdb.split('.')[0]
-            path_input_PDBs_pdbname = GUM.create_dir_tree(Paths.INPUT, 'PDBs', pdbname)
-            path_runscript_dest = GUM.create_dir_tree(path_input_PDBs_pdbname, 'FX_BuildModel')
-            pdbname_chain_fasta_dict = GUM.extract_pdbname_chain_fasta_from_pdbs(pdb, Paths.INPUT,
-                                                                                 write_wt_fastafiles, Paths.OUTPUT)
-            fx_mutant_name_list = self._make_fx_mutant_name_list(mutate_to_aa_list, pdbname_chain_fasta_dict)
-
-            try:
-                os.makedirs(path_runscript_dest)
-            except FileExistsError:
-                print('Part of all of path already exists. This is absolutely fine.')
+        def mutate_all_pdb(self, path_input, path_pdbfile, mutant_aa_list, path_output):
+            pdbname = path_pdbfile.split('/')[-1].split('.')[0]
+            path_input_pdbs_pdbname = GUM._os_makedirs(path_input, Paths.DIR_PDBS.value, pdbname)
+            path_runscript = GUM._os_makedirs(path_input_pdbs_pdbname, Paths.DIR_BMRUNSCRIPT)
+            pdbname_chain_fasta_dict = GUM.extract_pdbname_chain_fasta_from_pdbs(path_input, path_pdbfile,
+                                                                                 path_output, )
+            fx_mutant_name_list = self._make_fx_mutant_name_list(mutant_aa_list, pdbname_chain_fasta_dict)
 
             action = '<BuildModel>#,individual_list.txt'
-            GUM.write_runscript_for_pdbs(path_runscript_dest, 'RepairPDB_' + pdb, action)
+            GUM.write_runscript_for_pdbs(path_runscript, path_pdbfile, action)
 
             for fx_mutant_name in fx_mutant_name_list:
-                path_jobq_indivlist_dest = GUM.create_dir_tree(path_input_PDBs_pdbname, fx_mutant_name)
-
-                try:
-                    os.makedirs(path_jobq_indivlist_dest)
-                except FileExistsError:
-                    print('Part of all of path already exists. This is absolutely fine.')
-
-                try:
-                    os.chdir(path_jobq_indivlist_dest)
-                except ValueError:
-                    print('directory tree was not created')
-
+                path_jobq_indivlist_dest = GUM._os_makedirs(path_input_pdbs_pdbname, fx_mutant_name)
                 self._write_individual_list_for_mutant(fx_mutant_name, path_jobq_indivlist_dest)
-                # path_foldx = self.path_zeus_foldx_exe if use_cluster else self.path_local_foldx_exe
-                job_name = self.FXBM_jobname_prefix + fx_mutant_name
-                Cluster.write_job_q_bash(job_name, path_jobq_indivlist_dest)
-
-                if os.path.exists(os.path.join(path_runscript_dest, 'runscript.txt')):
-                    try:
-                        subprocess.call('qsub job.q', shell=True)
-                    except OSError:
-                        print('Problem with linux cp command.')
-
+                job_name = Paths.PREFIX_FX_BM.value + fx_mutant_name
+                Cluster.write_job_q_bash(job_name, path_job_q_dir=path_jobq_indivlist_dest,
+                                         path_runscript_dir=path_runscript)
+                if os.path.exists(os.path.join(path_runscript, 'runscript.txt')):
+                    Cluster.run_job_q(path_job_q_dir=path_jobq_indivlist_dest)
                 else:
                     raise ValueError('No runscript file was found')
 
@@ -121,5 +99,3 @@ class FoldX(object):
                               wt_repair_pdbname_1_ + _0_1_2_pdbs[2]
             action = '<AnalyseComplex>#'
             GUM.write_runscript_for_pdbs(path_to_runscript, pdbs_to_analyse, action)
-
-
