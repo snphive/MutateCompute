1a0fcbfc7b8001dde24e9bf50942fef5e26e0b08
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-09-22 14:59:30 +0200

    Convert improvised documentation (javadoc-like) to pycharm-supported docstring. Add type declarations in method arguments.

--- src/MutateFasta.py
+++ src/MutateFasta.py
@@ -12,25 +12,21 @@ class MutateFasta(object):
     def __init__(self, mutant_aa_list):
         self.__MUTANT_AA_LIST = mutant_aa_list
 
-    # Mutates FASTA (typically wild-type) sequences at every position, to every residue specified (typically all other
-    # 19 residues.)
-    #
-    # path_input            String      Abs path to input_data root dir to input fastafiles.
-    # fastafile_list        List        Where concurrency is not being used, the Schedular will pass the whole list of
-    #                                   fastafiles here.(NOTE: currently not being used, as Scheduler uses for loop).
-    # fastafile             String      Where concurrency is being used, the Scheduler will pass one Fastafile per
-    #                                   thread/process.
-    # mutant_aa_list        List        All residues that the sequence will be mutated to.
-    # write_1_fasta_only    Boolean     Write 1 fastafile for all mutants.
-    # write_fasta_per_mut   Boolean     Write 1 fastafile for every mutant.
-    # path_output           String      Abs path to output_data root dir, where mutants are written (fasta, csv, txt).
-    # write_csv             Boolean     True to write 1 csv file contain all mutants/. False by default.
-    # write_txt             Boolean     True to write 1 txt file contain all mutants. False by default.
-    #
-    # Returns a dictionary of the wt-title as key, dict as value which has evert mutant title as key, sequence as value.
-    # mutant_aa_list,
-    def mutate_every_residue(self, path_fastafile, write_1_fasta_only, write_fasta_per_mut, path_output_3dots,
-                             write_csv=False, write_txt=False):
+    def mutate_every_residue(self, path_fastafile: str, write_1_fasta_only: bool, write_fasta_per_mut: bool,
+                             path_output_3dots: str, write_csv=False, write_txt=False):
+        """
+        Mutates FASTA (typically wild-type) sequences at every position, to every residue specified, (typically the 19
+        other residues.)
+        :param path_fastafile: Absolute path to fasta file.
+        :param write_1_fasta_only: True writes 1 fasta file containing multiple fasta sequences (i.e. the mutants).
+        :param write_fasta_per_mut: True writes a fasta file for each fasta sequences (i.e. 1 per mutant).
+        :param path_output_3dots: Absolute path to a '3dot' subdirectory (i.e. 1...1000).
+        :param write_csv: True writes a csv file containing all mutants per wildtype fasta, (False by default).
+        :param write_txt: True writes a txt file containing all mutants per wildtype fasta, (False by default).
+        :return: Dictionary with the wild-type protein's name as key (e.g. 1_A), and another dictionary as the value.
+        The second dictionary has each mutant for this protein, with its name (e.g. R1A) as the key, the mutant's
+        sequence as the value.
+        """
         titleSeq = self.make_titleSeqDict_from_fastafile(path_fastafile)
         title_titleSeq = self.convert_titleSeqDict_to_titleTitleSeqDictDict(titleSeq)
         title_titleSeq_w_mutants = self._populate_title_titleSeq_with_mutants(title_titleSeq)
@@ -38,17 +34,23 @@ class MutateFasta(object):
                             write_csv, write_txt)
         return title_titleSeq_w_mutants
 
-    def mutate_every_residue_to_every_aa_write_1_file(self, path_fastafile):
+    def mutate_every_residue_to_every_aa_write_1_file(self, path_fastafile: str):
+        """
+
+        :param path_fastafile:
+        :return:
+        """
         path_output_3dots = GUM.make_root_fastas_3dots_dirs(Paths.OUTPUT, path_fastafile)
         self.mutate_every_residue(path_fastafile=path_fastafile, write_1_fasta_only=True, write_fasta_per_mut=False,
                                   path_output_3dots=path_output_3dots)
 
-    # title_titleSeq       Dictionary   Fasta title is the key. The value is mutant-title: mutated-sequence dictionary.
-    #                                   This already has the dict of wt-title: (wt-title:wt-sequence) dict.
-    # mutant_aa_list       List         All residues that the sequence(s) should be mutated to.
-    #
-    # Returns wt-title: mutant-title:mutant-sequence
-    def _populate_title_titleSeq_with_mutants(self, title_titleSeq):
+    def _populate_title_titleSeq_with_mutants(self, title_titleSeq: dict):
+        """
+    
+        :param title_titleSeq: Fasta title is the key. The value is mutant-title: mutated-sequence dictionary. This
+        already has the dict of wt-title: (wt-title:wt-sequence) dict.
+        :return: wt-title: mutant-title:mutant-sequence
+        """
         title_titleSeq_w_mutants = title_titleSeq
         for wt_title in list(title_titleSeq.keys()):
             wt_seq = title_titleSeq[wt_title][wt_title]
@@ -56,20 +58,16 @@ class MutateFasta(object):
                 title_titleSeq[wt_title], wt_title, wt_seq)
         return title_titleSeq_w_mutants
 
-    # A sequence is mutated at every position to every residue that is included in the list (mutant_aa_list).
-    #
-    # E.g.
-    # input as {'230498_A': 'MDVFM'}
-    # output as {'230498_A': 'MDVFM','M1A': 'ADVFM','M1C': 'CDVFM', etc.. }
-    #
-    # titleSeq          Dictionary      Already should have wt-title: wt-title:wt-sequence dictionary.
-    # wt_title          String          Name of fasta sequence (same as the fastafile name)
-    # wt_seq            String          Amino acid sequence in the fastafile (in FASTA format).
-    # mutant_aa_list    List            Specified list of residues, to mutate every residue in protein to.
-    #
-    # Returns a dictionary of sequence name as key, FASTA sequence itself as value, starting with wild-type and
-    # followed by every mutant according to a specified list of residues.
-    def _add_mutantTitle_mutatedSeq_to_dict(self, titleSeq, wt_title, wt_seq):
+    def _add_mutantTitle_mutatedSeq_to_dict(self, titleSeq: dict, wt_title: str, wt_seq: str):
+        """
+        A sequence is mutated at every position to every residue that is included in the list (mutant_aa_list).
+        E.g. input as {'230498_A': 'MDVFM'} output as {'230498_A': 'MDVFM','M1A': 'ADVFM','M1C': 'CDVFM', etc.. }
+        :param titleSeq: Already should have wt-title: wt-title:wt-sequence dictionary.
+        :param wt_title: Name of fasta sequence (same as the fastafile name).
+        :param wt_seq: Amino acid sequence in the fastafile (in FASTA format).
+        :return: Dictionary of sequence name as key, FASTA sequence itself as value, starting with wild-type and
+        followed by every mutant according to a specified list of residues.
+        """
         titleSeqDict_w_mutants = {wt_title: titleSeq[wt_title]}
         mutable_seq = MutableSeq(wt_seq, IUPAC.protein)
         for i, wt_aa in enumerate(mutable_seq):
@@ -82,23 +80,23 @@ class MutateFasta(object):
                     mutable_seq[i] = wt_aa_at_i
         return titleSeqDict_w_mutants
 
-    # Writes the mutants out to fastafiles and/or csv files and/or txt files.
-    # These can be written in one file containing all mutants or one file per mutant.
-    # The fastafiles are written to /output_data/<fastafilename>/mutants/.
-    # The csvfiles and txtfiles are written to /output_data/<fastafilename/sequences/.
-    # The reason for fastafile mutants being written to an input folder is that these sequences are generated as direct
-    # inputs for the mutation operation.
-    #
-    # title_titleSeq_w_mutants      Dictionary  Title of wild-type associated to every mutant title:sequence
-    #                                           starting with the wild-type title:sequence pair.
-    # write_1_fasta_only            Boolean     True to write one fastafile containing all mutants, separated by \n.
-    # write_fasta_per_mut           Boolean     True to write one fastafile per mutant.
-    # path_output_3dots             String      Absolute path of output_data dir (where fasta, txt, csv written),
-    #                                           includes to subdirs: /fastas/xxxx...yyyy/ e.g. 1001...2000
-    # write_csv                     Boolean     True to write 1 csv file for wt & mutants. False by default.
-    # write_txt                     Boolean     True to write 1 txt file for wt & mutants. False by default.
-    def _write_mutants(self, title_titleSeq_w_mutants, write_1_fasta_only, write_fasta_per_mut, path_output_3dots,
-                       write_csv=False, write_txt=False):
+    def _write_mutants(self, title_titleSeq_w_mutants: dict, write_1_fasta_only: bool, write_fasta_per_mut: bool,
+                       path_output_3dots: str, write_csv=False, write_txt=False):
+        """
+        Writes the mutants out to fastafiles and/or csv files and/or txt files. These can be written in one file
+        containing all mutants or one file per mutant.
+        The fastafiles are written to /output_data/<fastafilename>/mutants/.
+        The csvfiles and txtfiles are written to /output_data/<fastafilename/sequences/.
+        The reason for fastafile mutants being written to an input folder is that these sequences are generated as
+        direct inputs for the mutation operation.
+        :param title_titleSeq_w_mutants: Title of wild-type associated to every mutant title:sequence.
+        :param write_1_fasta_only: True to write one fastafile containing all mutants, separated by \n.
+        :param write_fasta_per_mut: True to write one fastafile per mutant.
+        :param path_output_3dots: Absolute path of output_data dir (where fasta, txt, csv written), includes to
+        subdirs: /fastas/xxxx...yyyy/ e.g. 1001...2000
+        :param write_csv: True to write 1 csv file for wt & mutants. False by default.
+        :param write_txt: True to write 1 txt file for wt & mutants. False by default.
+        """
         path_1_fastafile = None
         path_1_fastafile_open = None
         path_fastafilepermut = None
@@ -151,29 +149,22 @@ class MutateFasta(object):
         if path_seqscsv_open is not None:
             path_seqscsv_open.close()
 
-    # Splits a fastafile's text contents into a dictionary. The key is fastafile's title, value is (amino acid)
-    # sequence.
-    # E.g.
-    # ">1_A\n'RVYLTFDELRETKTSEYFSLSHHPLDYRILLMDEDQDRIYVG...' etc"
-    #
-    # is saved as
-    #
-    # {
-    #   '1_A': <seq of 1_A>,
-    #   '2_A': <seq of 2_A>,
-    #    etc..
-    # }
-    #
-    #
-    # The fastafile is assumed to be either two lines: 1st line ">title", 2nd line sequence, but the method also allows
-    # there to be either no title (i.e. sequence only) or an empty title (">" with no title after it). In these both
-    # scenarios, the title is created based on the name of the file itself.
-    #
-    # path_fastafiles   String OR List of Strings   Absolute path to the fasta file, including the file (incl. .fasta)
-    #                                               e.g. ~/../input_data/fastas/1_A/1_A.fasta
-    #
-    # Returns title:sequence dictionary.
-    def make_titleSeqDict_from_fastafile(self, path_input_fastafiles):
+    def make_titleSeqDict_from_fastafile(self, path_input_fastafiles: str):
+        """
+        Splits a fastafile's text contents into a dictionary. The key is fastafile's title, value is (amino acid)
+        sequence. E.g. ">1_A\n'RVYLTFDELRETKTSEYFSLSHHPLDYRILLMDEDQDRIYVG...' etc" is saved as
+            {
+               '1_A': <seq of 1_A>,
+               '2_A': <seq of 2_A>,
+               etc..
+            }
+        The fasta file is assumed to be either two lines: 1st line ">title", 2nd line sequence, but the method also
+        allows there to be either no title (i.e. sequence only) or an empty title (">" with no title after it). In these
+        both scenarios, the title is created based on the name of the file itself.
+        :param path_input_fastafiles: Absolute path to the fasta file, including the file (incl. .fasta)
+        e.g. ~/../input_data/fastas/1_A/1_A.fasta
+        :return: title:sequence dictionary.
+        """
         title_sequence_dict = {}
         if isinstance(path_input_fastafiles, str):
             path_input_fastafiles = [path_input_fastafiles]
@@ -190,46 +181,44 @@ class MutateFasta(object):
                 title_sequence_dict[title] = fasta_seq
         return title_sequence_dict
 
-    # Converts a title:sequence dictionary to a title: {title:seq dictionary} dictionary. One use of this is to add
-    # 19 more titles per residue for different titles which represent point mutants.
-    # E.g.
-    #
-    # {
-    #   '1_A': <seq of 1_A>,
-    #   '2_A': <seq of 2_A>,
-    #    etc..
-    # }
-    #
-    # is converted to
-    #
-    # {
-    #   '1_A':
-    #           {'1_A'      : <seq of 1_A>,
-    #            '1_A_R1A'  : <seq of 1_A_R1A>, ... etc },
-    #   '2_A':
-    #           {'2_A'      : <seq of 2_A>,
-    #            '2_A_P1A'  : <seq of 2_A_P1A>, ... etc },
-    #    etc..
-    # }
-    #
-    # title_sequence_dict   Dictionary   Fasta title (i.e. sequence id) is key. Sequence in FASTA format is value.
-    #
-    # Returns dictionary of title: title:sequence dictionaries.
-    def convert_titleSeqDict_to_titleTitleSeqDictDict(self, title_sequence_dict):
+    def convert_titleSeqDict_to_titleTitleSeqDictDict(self, title_sequence_dict: dict):
+        """
+        Converts a title:sequence dictionary to a title: {title:seq dictionary} dictionary. One use of this is to add
+        19 more titles per residue for different titles - representing point mutants.
+        E.g.
+            {
+              '1_A': <seq of 1_A>,
+              '2_A': <seq of 2_A>,
+               etc..
+            }
+            is converted to
+            {
+              '1_A':
+                      {'1_A'      : <seq of 1_A>,
+                       '1_A_R1A'  : <seq of 1_A_R1A>, ... etc },
+              '2_A':
+                      {'2_A'      : <seq of 2_A>,
+                       '2_A_P1A'  : <seq of 2_A_P1A>, ... etc },
+               etc..
+            }
+        :param title_sequence_dict: Fasta title (i.e. sequence id) is key. Sequence in FASTA format is value.
+        :return: Dictionary of title: title:sequence dictionaries.
+        """
         title_titleSeqDict_dict = {}
         for title in title_sequence_dict.keys():
             title_titleSeqDict_dict[title] = title_sequence_dict
         return title_titleSeqDict_dict
 
-    # Builds list of absolute path(s) for fastafile or fastafiles in list, by adding an extra directory between the
-    # path_input and the file itself, named exactly the same as the file.
-    # E.g. input_data/fastas/<fastaname>/fastafile.
-    #
-    # path_input        String              Absolute path of directory holding list of fasta files.
-    # fastafiles        String OR List      Target fasta files (including extensions).
-    #
-    # Returns list of fasta files with absolute path to them, e.g. ~/.../input_data/fastas/1_A/1_A.fasta
     def build_complete_paths_for_input_fastas(self, path_input, fastafiles):
+        """
+        Builds list of absolute path(s) for fastafile or fastafiles in list, by adding an extra directory between the
+        path_input and the file itself, named exactly the same as the file.
+        E.g. input_data/fastas/<fastaname>/fastafile.
+
+        :param path_input: Absolute path of directory holding list of fasta files.
+        :param fastafiles: Target fasta files (including extensions).
+        :return: List of fasta files with absolute path to them, e.g. ~/.../input_data/fastas/1_A/1_A.fasta
+        """
         if isinstance(fastafiles, str):
             fastafiles = [fastafiles]
         return [path_input + '/' + Paths.DIR_FASTAS.value + '/' + fastafile.split('.')[0] + '/' + fastafile for
