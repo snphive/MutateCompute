61421ed16a385fb1c2168cf13f23f23330c9b2a5
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-10-12 13:12:31 +0200

    Update/tidy up this module. Add description comment at head.

--- src/KickOffManual.py
+++ src/KickOffManual.py
@@ -3,34 +3,32 @@ import glob
 import natsort
 from src.Main import Main
 from src.Paths import Paths
-import pydevd
-pydevd.settrace('localhost', port=51234, stdoutToServer=True, stderrToServer=True)
+from src.AminoAcids import AA
+# import pydevd
+# pydevd.settrace('localhost', port=51234, stdoutToServer=True, stderrToServer=True)
 
-if len(sys.argv) < 2:
-    use_cluster = False
-else:
-    use_cluster = True if sys.argv[1] == 'use_cluster=True' else False
+"""
+This module is for doing manual runs, either to check something works or just to do a single input rather than a batch
+which is more for KickOff.py.
+I am manually setting the use_cluster and path_inputs.. variables here but this is not essential.  
+"""
 
+
+use_cluster = False
 Paths.set_up_paths(use_cluster)
 
-operations = {'do_mutate_fasta': False, 'do_agadir': True,
+operations = {'do_mutate_fasta': True, 'do_agadir': False,
               'do_foldx_repair': False, 'do_foldx_buildmodel': False,
               'do_foldx_stability': False, 'do_foldx_analysecomplex': False}
 use_multithread = False
+path_input_fastas_dir = '/Users/u0120577/REPO_PDB_FASTA/fastas/29611_fastas_1000/1...1000'
 
-
-# path_input_fastas_dir = Paths.MC_REPO_FASTAS.value + '/fastas_1000_rest'
-if use_cluster:
-    path_input_fastas_dir = '/switchlab/group/shazib/SnpEffect/input_data/mutants_multifastas/1...2'
-else:
-    path_input_fastas_dir = '/Users/u0120577/PycharmProjects/input_data/mutants_multifastas/1...2'
-path_fastafiles = natsort.natsorted(glob.glob(path_input_fastas_dir + '/**/*.fasta', recursive=True))
+path_fastafiles = sorted(glob.glob(path_input_fastas_dir + '/**/*.fasta', recursive=True))
 if not path_fastafiles:
     raise ValueError('No fasta files to process. Check paths are correct and check files are where you expect.')
-
 path_pdbfiles = []
+main = Main(operations, use_multithread, Paths.INPUT, Paths.OUTPUT, path_pdbfiles, path_fastafiles,
+            AA.LIST_ALL_20_AA.value)
 
-main = Main(operations, use_multithread, Paths.INPUT, Paths.OUTPUT, path_pdbfiles, path_fastafiles, ['A', 'C'])
-            # AA.LIST_ALL_20_AA.value)
+# pydevd.stoptrace()
 
-pydevd.stoptrace()
