e5fd36ebe5365948ed059e7d1a946390b4762b37
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-10-17 00:16:33 +0200

    Remove pdb if pdb exists.

--- src/RemoveFilesManual.py
+++ src/RemoveFilesManual.py
@@ -4,11 +4,11 @@ from src.GeneralUtilityMethods import GUM
 from src.Conditions import Cond
 import os
 from os import walk
-import pydevd
-pydevd.settrace('localhost', port=51234, stdoutToServer=True, stderrToServer=True)
+# import pydevd
+# pydevd.settrace('localhost', port=51234, stdoutToServer=True, stderrToServer=True)
 
 fx_mutant_dirs = []
-pdbname = 'RepairPDB_1'
+pdbname = 'RepairPDB_2'
 for (dirpath, dirnames, filenames) in walk(os.path.join(Paths.SE_OUTPUT_BM.value, pdbname)):
     fx_mutant_dirs.extend(dirnames)
     break
@@ -17,7 +17,8 @@ for fx_mutant_dir in fx_mutant_dirs:
     bm = FoldX().BuildModel(Cond.INCELL_MAML_FX.value)
     path_output_pdbname_mutant = os.path.join(Paths.SE_OUTPUT_BM.value, pdbname, fx_mutant_dir)
     bm.remove_config_files(path_output_pdbname_mutant)
-    path_pdbfile = os.path.join(Paths.SE_OUTPUT_BM.value, pdbname + '.pdb')
-    GUM.linux_remove_file(path_pdbfile)
+    path_pdbfile = os.path.join(path_output_pdbname_mutant, pdbname + '.pdb')
+    if os.path.exists(path_pdbfile):
+        GUM.linux_remove_file(path_pdbfile)
 
-pydevd.stoptrace()
+# pydevd.stoptrace()
