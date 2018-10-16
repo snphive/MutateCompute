f1a175e4461554c503991ca6c9617292c9b2d59b
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-10-16 15:39:21 +0200

    Add logic to remove used config files from foldx output folders.

--- src/RemoveFilesManual.py
+++ src/RemoveFilesManual.py
@@ -1,6 +1,16 @@
 from src.FoldX import FoldX
 from src.Paths import Paths
+import os
+from os import walk
+import pydevd
+pydevd.settrace('localhost', port=51234, stdoutToServer=True, stderrToServer=True)
 
+fx_mutant_dirs = []
+for (dirpath, dirnames, filenames) in walk(os.path.join(Paths.SE_OUTPUT_BM.value, 'RepairPDB_1')):
+    fx_mutant_dirs.extend(dirnames)
+    break
 
-path_output_pdbname_mutant = Paths.SE_OUTPUT_FASTAS
-FoldX.BuildModel.remove_config_files()
+for fx_mutant_dir in fx_mutant_dirs:
+    FoldX.BuildModel.remove_config_files(fx_mutant_dir)
+
+pydevd.stoptrace()
