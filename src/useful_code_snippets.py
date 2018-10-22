8bcf36e8d87ae0fe00d6e053b3e7a637999b2fc4
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-10-22 17:41:21 +0200

    Move unused but potentially useful code to useful_code_snippets

--- src/useful_code_snippets.py
+++ src/useful_code_snippets.py
@@ -23,3 +23,34 @@ print('Time taken to complete iterating through fasta_list (after methods have b
 # import pydevd
 # pydevd.settrace('localhost', port=51234, stdoutToServer=True, stderrToServer=True)
 # pydevd.stoptrace()
+
+
+# Might be better to log in manually, as this can hang if mulitplexing is used.
+# @staticmethod
+# def log_in_cluster_ssh(*modules):
+# HOST = "zeus.psb.ugent.be"
+# # Ports are handled in ~/.ssh/config since we use OpenSSH
+# COMMAND = "-p 7788 shazib"
+# Ports are handled in ~/.ssh/config since we use OpenSSH
+# ssh = subprocess.Popen(["ssh", "%s" % HOST, COMMAND], shell=False, stdout=subprocess.PIPE,
+# stderr=subprocess.PIPE)
+# result = ssh.stdout.readlines()
+# if result == []:
+#     error = ssh.stderr.readlines()
+#     print("ERROR: %s" % error, file=sys.stderr)
+# else:
+#     print(result)
+
+# or use fabric??
+# import os
+# from fabric.api import *
+#
+# HOST="www.example.org"
+# COMMAND="uname -a"
+#
+# env.user = os.getenv('SSH_USER', 'vagrant')
+# env.password = os.getenv('SSH_PASSWORD', 'vagrant')
+#
+# @hosts(HOST)
+# def do_something():
+#     run(COMMAND)
