0a0e9eaefaae7c82c5f8114ec542f158f79dda86
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-10-25 17:45:36 +0200

    Remove already written unwanted PSX_globalresidue.out files from the output agadir folder.

--- src/RemoveFilesManual.py
+++ src/RemoveFilesManual.py
@@ -2,23 +2,50 @@ from src.FoldX import FoldX
 from src.Paths import Paths
 from src.GeneralUtilityMethods import GUM
 from src.Conditions import Cond
+from src.Str import Str
 import os
+import glob
+import sys
+import subprocess
 from os import walk
 # import pydevd
 # pydevd.settrace('localhost', port=51234, stdoutToServer=True, stderrToServer=True)
 
-fx_mutant_dirs = []
-pdbname = 'RepairPDB_2'
-for (dirpath, dirnames, filenames) in walk(os.path.join(Paths.SE_OUTPUT_BM.value, pdbname)):
-    fx_mutant_dirs.extend(dirnames)
-    break
+# fx_mutant_dirs = []
+# pdbname = 'RepairPDB_2'
+# for (dirpath, dirnames, filenames) in walk(os.path.join(Paths.SE_OUTPUT_BM.value, pdbname)):
+#     fx_mutant_dirs.extend(dirnames)
+#     break
+#
+# for fx_mutant_dir in fx_mutant_dirs:
+#     bm = FoldX().BuildModel(Cond.INCELL_MAML_FX.value)
+#     path_output_pdbname_mutant = os.path.join(Paths.SE_OUTPUT_BM.value, pdbname, fx_mutant_dir)
+#     bm.remove_config_files(path_output_pdbname_mutant)
+#     path_pdbfile = os.path.join(path_output_pdbname_mutant, pdbname + '.pdb')
+#     if os.path.exists(path_pdbfile):
+#         GUM.linux_remove_file(path_pdbfile)
 
-for fx_mutant_dir in fx_mutant_dirs:
-    bm = FoldX().BuildModel(Cond.INCELL_MAML_FX.value)
-    path_output_pdbname_mutant = os.path.join(Paths.SE_OUTPUT_BM.value, pdbname, fx_mutant_dir)
-    bm.remove_config_files(path_output_pdbname_mutant)
-    path_pdbfile = os.path.join(path_output_pdbname_mutant, pdbname + '.pdb')
-    if os.path.exists(path_pdbfile):
-        GUM.linux_remove_file(path_pdbfile)
+
+Paths.set_up_paths(use_cluster=(len(sys.argv) > 1 and sys.argv[1].strip(' ') == 'use_cluster=True'))
+use_multithread = False
+# path_fastafiles = sorted(glob.glob(path_input_fastas_dir + '/**/*.fasta', recursive=True))
+path_output_agadir_globresidfiles = Paths.OUTPUT_AGADIR + '/1...250/**/' + Str.GLOBRESIDUE_OUT.value
+path_globalresidueout_files = sorted(glob.glob(path_output_agadir_globresidfiles, recursive=True))
+if not path_globalresidueout_files:
+    raise ValueError('No PSX_globalresidue.out files to delete. Check paths are correct and check files are where you '
+                     'expect.')
+for path_globalresidueout_file in path_globalresidueout_files:
+    # GUM.linux_remove_file(path_globalresidueout_file)
+    if not os.path.exists(path_globalresidueout_file):
+        print('Path: ' + path_globalresidueout_file + ' does not exist.')
+    else:
+        cmd = 'rm' + Str.SPCE.value + path_globalresidueout_file
+        try:
+            subprocess.call(cmd, shell=True)
+        except FileNotFoundError as fnf:
+            print('File not found: ' + path_globalresidueout_file)
+            print(fnf)
+        except OSError:
+            print(Str.PROBLNXCMD_MSG.value + cmd)
 
 # pydevd.stoptrace()
