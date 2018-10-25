e831457835fdbbce6b05b361cd64780934126540
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-10-25 17:48:15 +0200

    RemoveFiles bash script to run locally.

--- bash/RemoveFiles.sh
+++ bash/RemoveFiles.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-source /home/shazib/.bash_profile
-source /switchlab/group/shazib/SnpEffect/venv/bin/activate
-
-export PYTHONPATH=$PYTHONPATH:/switchlab/group/shazib/SnpEffect/venv/pycharm-debug.egg
-export PYTHONPATH=$PYTHONPATH:/switchlab/group/shazib/SnpEffect/venv/lib/python3.7/site-packages
-export PYTHONPATH=$PYTHONPATH:/switchlab/group/shazib/SnpEffect
+source /Users/u0120577/.profile
+source /Users/u0120577/PycharmProjects/MutateCompute/venv/bin/activate
 
 #!/usr/bin/env python3
-python3 /switchlab/group/shazib/SnpEffect/src/RemoveFilesManual.py 'use_cluster=True'
+python3 ./RemoveFiles.py 'use_cluster=False'
