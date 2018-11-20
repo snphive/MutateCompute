60bad67c01ceb78752627dd7b164688376bbb064
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-11-20 14:20:10 +0100

    Edit to include launcher for making tarballs. (Edit name of module to reflect this additional action).

--- src/DeleteMoveFilesManual.py
+++ src/DeleteMoveFilesManual.py
@@ -8,6 +8,7 @@ import os
 import glob
 import sys
 import subprocess
+from src.OutputsParser import Parser
 # import pydevd
 # pydevd.settrace('localhost', port=51234, stdoutToServer=True, stderrToServer=True)
 
@@ -28,28 +29,39 @@ Paths.set_up_paths(use_cluster=(len(sys.argv) > 1 and sys.argv[1].strip(' ') == 
 """
 Select files for deletion.
 """
-path_output_agadir_globresidfiles = Paths.OUTPUT_AGADIR + '/1...250/**/' + Str.GLOBRESIDUE_OUT.value
-path_globalresidueout_files = sorted(glob.glob(path_output_agadir_globresidfiles, recursive=True))
-if not path_globalresidueout_files:
-    raise ValueError('No PSX_globalresidue.out files to delete. Check paths are correct and check files are where you '
-                     'expect.')
+# path_output_agadir_globresidfiles = Paths.OUTPUT_AGADIR + '/1...250/**/' + Str.GLOBRESIDUE_OUT.value
+# path_globalresidueout_files = sorted(glob.glob(path_output_agadir_globresidfiles, recursive=True))
+# if not path_globalresidueout_files:
+#     raise ValueError('No PSX_globalresidue.out files to delete. Check paths are correct and check files are where you '
+#                      'expect.')
 
 """
 Delete files.
 """
-for path_globalresidueout_file in path_globalresidueout_files:
-    # GUM.linux_remove_file(path_globalresidueout_file)
-    if not os.path.exists(path_globalresidueout_file):
-        print('Path: ' + path_globalresidueout_file + ' does not exist.')
-    else:
-        cmd = 'rm' + Str.SPCE.value + path_globalresidueout_file
-        try:
-            subprocess.call(cmd, shell=True)
-        except FileNotFoundError as fnf:
-            print('File not found: ' + path_globalresidueout_file)
-            print(fnf)
-        except OSError:
-            print(Str.PROBLNXCMD_MSG.value + cmd)
+# for path_globalresidueout_file in path_globalresidueout_files:
+#     # GUM.linux_remove_file(path_globalresidueout_file)
+#     if not os.path.exists(path_globalresidueout_file):
+#         print('Path: ' + path_globalresidueout_file + ' does not exist.')
+#     else:
+#         cmd = 'rm' + Str.SPCE.value + path_globalresidueout_file
+#         try:
+#             subprocess.call(cmd, shell=True)
+#         except FileNotFoundError as fnf:
+#             print('File not found: ' + path_globalresidueout_file)
+#             print(fnf)
+#         except OSError:
+#             print(Str.PROBLNXCMD_MSG.value + cmd)
+
+"""
+Select directory for packing into tar.
+"""
+
+path_dir_txt_files_to_pack = os.path.join(Paths.OUTPUT_AGADIR, '02064053-3f32-32e6-9660-aaaffc30db87')
+
+"""
+Pack files in directory into tar.
+"""
+Parser().make_tarfile(path_dir_txt_files_to_pack)
 
 
 # Running FoldX BuildModel and deleting config files immediately after running.
