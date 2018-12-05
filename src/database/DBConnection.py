5342acee939e42b4acd4a087fe5b33dc9fa23857
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-12-05 20:04:54 +0100

    Add logic for making database connection/disconnection. (Temporarily has code for making table, columns, inserting values - which will likely be moved to another class)

--- src/database/DBConnection.py
+++ src/database/DBConnection.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from src.enums.UNPW import Server
+from src.enums.DBServer import Server
 
 
 class DBCnx(object):
@@ -14,46 +14,40 @@ class DBCnx(object):
                                                      database=Server.PRACTICE_DB_MYSQL_DBNAME.value,
                                                      port=Server.PRACTICE_DB_MYSQL_PORT.value)
             cur = mysql_practice.cursor()
-            cur.execute("CREATE TABLE testDBCon2 ( id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, title TEXT NOT NULL )")
+            cur.execute("CREATE TABLE IF NOT EXISTS testDBCnx ( pdb VARCHAR(200) NOT NULL PRIMARY KEY, mutation VARCHAR(20) NOT "
+                        "NULL )")
         except mysql.connector.Error as err:
             print("Something went wrong: {}".format(err))
+
+    def add_new_columns(self, column_name: str, data_type_defin: str):
+        global mysql_practice
+        cur = mysql_practice.cursor()
+        cur.execute("ALTER TABLE testDBCnx ADD COLUMN ddG FLOAT AFTER title")
+
+    def insert_values(self):
+        global mysql_practice
+        cur = mysql_practice.cursor()
+        cur.execute("INSERT INTO testDBCnx (pdb, mutation, ddG) VALUES ('RepairPDB_1', 'AA101A', 0.002)")
+    #     may need to include schema name, e.g. testForScope.testDBCnx
+
+    def disconnect_mysql_practice_db(self):
+        global mysql_practice
+        mysql_practice.close()
 
     def connect_mysql_snpeffect_v5_db(self):
         global mysql_snpeffect5
         try:
-            # cnx = mysql.connector.connect(user='root', password='K0yGrJ8(', host='127.0.0.1', database='mydb', port='3306')
             mysql_snpeffect5 = mysql.connector.connect(user=Server.SNPEFFECT_V5_MYSQL_USER.value,
                                                        password=Server.SNPEFFECT_V5_MYSQL_PW.value,
                                                        host=Server.SNPEFFECT_V5_MYSQL_NETADD.value,
                                                        database=Server.SNPEFFECT_V5_MYSQL_DBNAME.value,
                                                        port=Server.SNPEFFECT_V5_MYSQL_PORT.value)
             cur = mysql_snpeffect5.cursor()
-            cur.execute("CREATE TABLE testDBCon2 ( id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, title TEXT NOT NULL )")
+            cur.execute("CREATE TABLE testDBConx ( id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, title TEXT NOT NULL )")
 
         except mysql.connector.Error as err:
             print("Something went wrong: {}".format(err))
 
-    def disconnect_mysql_practice_db(self):
-        global mysql_practice
-        mysql_practice.close()
-
     def disconnect_mysql_snpeffect_v5_db(self):
         global mysql_snpeffect5
         mysql_snpeffect5.close()
-
-
-# """
-# Read ddG from csv file and write to database.
-# :param path_output_bm_pdb_fxmutant_ddG_csvfile:
-# """
-# connection = mysql.connector.connect(user=Server.SNPEFFECT_V5_MYSQL_UN.value,
-#                                      password=Server.SNPEFFECT_V5_MYSQL_PW.value,
-#                                      host=Server.SNPEFFECT_V5_MYSQL_NETADD.value)
-# , database='snpeffect_v5', port='3306')
-# cursor = connection.cursor()
-# cursor.execute("SHOW DATABASES")
-# for x in cursor:
-#     print(x)
-# cursor.execute("CREATE DATABASE SnpEffect_v5.0")
-# cursor.execute(
-#     "CREATE TABLE testingDBConnection ( id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, title TEXT NOT NULL )")
