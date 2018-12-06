660081aef8d5e396b648f601ef4bbef364884e99
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-12-06 15:33:00 +0100

    Make table, add columns, add values to columns.

--- src/database/DBConnection.py
+++ src/database/DBConnection.py
@@ -1,39 +1,78 @@
 import mysql.connector
 from src.enums.DBServer import Server
+"""
+Class for connecting to database and performing certain sql actions.
+"""
 
 
 class DBCnx(object):
 
     def connect_mysql_practice_db(self):
+        """
+        This method works. The table was created in
+        :return:
+        """
+
         global mysql_practice
         try:
-            # cnx = mysql.connector.connect(user='root', password='K0yGrJ8(', host='127.0.0.1', database='mydb', port='3306')
             mysql_practice = mysql.connector.connect(user=Server.PRACTICE_DB_MYSQL_USER.value,
                                                      password=Server.PRACTICE_DB_MYSQL_PW.value,
                                                      host=Server.PRACTICE_DB_MYSQL_NETADD.value,
                                                      database=Server.PRACTICE_DB_MYSQL_DBNAME.value,
                                                      port=Server.PRACTICE_DB_MYSQL_PORT.value)
-            cur = mysql_practice.cursor()
-            cur.execute("CREATE TABLE IF NOT EXISTS testDBCnx ( pdb VARCHAR(200) NOT NULL PRIMARY KEY, mutation VARCHAR(20) NOT "
-                        "NULL )")
+            cursor = mysql_practice.cursor()
+            cursor.execute("CREATE TABLE IF NOT EXISTS fx_data ( pdb VARCHAR(200) NOT NULL PRIMARY KEY, mutation VARCHAR(20) "
+                           "NOT NULL )")
         except mysql.connector.Error as err:
             print("Something went wrong: {}".format(err))
+            mysql_practice.rollback()
+        # finally:
+        #     mysql_practice.close()
 
     def add_new_columns(self, column_name: str, data_type_defin: str):
         global mysql_practice
-        cur = mysql_practice.cursor()
-        cur.execute("ALTER TABLE testDBCnx ADD COLUMN ddG FLOAT AFTER title")
+        cursor = mysql_practice.cursor()
+        try:
+            cursor.execute("ALTER TABLE fx_data ADD COLUMN ddG FLOAT AFTER mutation")
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            mysql_practice.rollback()
 
-    def insert_values(self):
+    def insert_values_by_column(self, columns: list, values: list):
         global mysql_practice
-        cur = mysql_practice.cursor()
-        cur.execute("INSERT INTO testDBCnx (pdb, mutation, ddG) VALUES ('RepairPDB_1', 'AA101A', 0.002)")
-    #     may need to include schema name, e.g. testForScope.testDBCnx
+        cursor = mysql_practice.cursor()
+        sql_cmd = ' INSERT INTO fx_data(pdb, mutation, ddG) VALUES(\"RepairPDB_5\", \"AA101A\", \"0.00023\") '
+        try:
+            # columns = ','.join(columns)
+            # values = ','.join([str(value) for value in values])
+            # values = [('"' + value + '"') for value in values])
+            # sql_cmd = f'INSERT INTO testForScope.testDBCnx ({columns}) VALUES ({values});'
+            # cursor.execute(sql_cmd)
+
+            # abc = 'RepairPDB_3'
+            # sql = f'INSERT INTO testForScope.testDBCnx (pdb) VALUES ({abc})'
+            # sql_insery = """ sql """
+            # cursor.execute(sql_insery)
+            # pdbname =
+            # cursor.execute("SHOW DATABASES")
+            # for x in cur:
+            #     print(x)
+            # cursor.execute("SELECT * FROM ")
+            cursor.execute(sql_cmd)
+            mysql_practice.commit()
+
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            mysql_practice.rollback()
 
     def disconnect_mysql_practice_db(self):
         global mysql_practice
+        print("Disconnecting mysql practice db")
         mysql_practice.close()
 
+    """
+    CONNECTING AND PERFORMING SQL ON SNP_EFFECT_V5
+    """
     def connect_mysql_snpeffect_v5_db(self):
         global mysql_snpeffect5
         try:
@@ -42,12 +81,16 @@ class DBCnx(object):
                                                        host=Server.SNPEFFECT_V5_MYSQL_NETADD.value,
                                                        database=Server.SNPEFFECT_V5_MYSQL_DBNAME.value,
                                                        port=Server.SNPEFFECT_V5_MYSQL_PORT.value)
-            cur = mysql_snpeffect5.cursor()
-            cur.execute("CREATE TABLE testDBConx ( id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, title TEXT NOT NULL )")
+            cursor = mysql_snpeffect5.cursor()
+            cursor.execute("CREATE TABLE testDBConx ( id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, title TEXT NOT NULL )")
 
         except mysql.connector.Error as err:
             print("Something went wrong: {}".format(err))
+            mysql_snpeffect5.rollback()
+        # finally:
+        #     mysql_snpeffect5.close()
 
     def disconnect_mysql_snpeffect_v5_db(self):
         global mysql_snpeffect5
+        print("Disconnecting mysql snpeffect_v5")
         mysql_snpeffect5.close()
