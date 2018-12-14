ef312edca656b271f18ca22efa5361819efb3893
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-12-14 17:46:47 +0100

    Remove pdb files after buildmodel computation but only if it has 1 chain (hence no complex).

--- src/cluster_py_scripts/run_remove_files_zeus.py
+++ src/cluster_py_scripts/run_remove_files_zeus.py
@@ -5,7 +5,9 @@ Script for removing unnecessary FoldX input & output files from within cluster, 
 import os
 import sys
 from src.enums.Paths import Paths
+from src.enums.Str import Str
 from src.FoldX import FoldX
+from src.tools.GeneralUtilityMethods import GUM
 from src.enums.Conditions import Cond
 
 __author__ = "Shahin Zibaee"
@@ -27,21 +29,26 @@ Delete files no longer needed from the output folders in order to reduce disk sp
 ac_or_bm = path_output_ac_or_bm_pdb_fxmutant_dir.split('/')[-3]
 pdbname = path_output_ac_or_bm_pdb_fxmutant_dir.split('/')[-2]
 fxmutantname = path_output_ac_or_bm_pdb_fxmutant_dir.split('/')[-1]
-print('run_remove_files_zeus.py ###################################')
+print('Run_remove_files_zeus.py ###################################')
 fx = FoldX()
 if ac_or_bm == Paths.DIR_AC.value:
     ac = fx.AnalyseComplex(Cond.INCELL_MAML_FX.value)
-    print('remove pdb files from bm folders')
+    print('Remove pdb files from bm folders, (as interaction energies have been calculated.)')
     fx.rm_pdbfiles(os.path.join(Paths.OUTPUT_BM, pdbname, fxmutantname))
-    print('remove pdb files from ac folders')
+    print('Remove pdb files from ac folders.')
     fx.rm_pdbfiles(os.path.join(path_output_ac_or_bm_pdb_fxmutant_dir))
-    print('remove any unwanted extra summary files')
+    print('Remove any unwanted extra summary files from ac folders.')
     ac.rm_all_sumry_except_1_0(path_output_ac_or_bm_pdb_fxmutant_dir)
 elif ac_or_bm == Paths.DIR_BM.value:
     bm = fx.BuildModel(Cond.INCELL_MAML_FX.value)
-print('remove config files')
+    path_pdbfile = os.path.join(path_output_ac_or_bm_pdb_fxmutant_dir, pdbname + Str.PDBEXT.value)
+    if GUM.get_num_of_chains(path_pdbfile) == 1:
+        # BuildModel has no use for these mutant pdb files and because this pdb only has 1 chain, AnalyseComplex has
+        # no use for them either. Hence, they can already be removed.
+        fx.rm_pdbfiles(path_output_ac_or_bm_pdb_fxmutant_dir)
+print('Remove config files from ac or bm folders.')
 fx.rm_config_files(path_output_ac_or_bm_pdb_fxmutant_dir)
-print('remove logfiles from ac folders')
+print('Remove logfiles from ac or bm folders.')
 fx.rm_logfiles(path_output_ac_or_bm_pdb_fxmutant_dir)
-print('remove unnecessary fxout files from ac folders')
+print('Remove unnecessary fxout files from ac or bm folders.')
 fx.rm_unnecessary_fxoutfiles(path_output_ac_or_bm_pdb_fxmutant_dir)
