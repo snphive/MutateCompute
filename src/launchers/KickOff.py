44936ddf46b9d70b0b7b4654ea8449349deed8cd
Shahin Zibaee <shahinzibaee@hotmail.com>
2018-12-17 17:52:19 +0100

    Don't waste time proceeding to look for files to delete for a wild-type to wild-type residue (which is listed in the specifc-fxmutants list) because the foldx logic is now set up to not waste time computing these. Change to mutant directory for running remove on cluster so that cluster logfiles will be written there.

--- src/launchers/KickOff.py
+++ src/launchers/KickOff.py
@@ -21,8 +21,8 @@ from src.enums.Conditions import Cond
 from src.Main import Main
 from src.FoldX import FoldX
 from src.Cluster import Cluster
-import pydevd
-pydevd.settrace('localhost', port=51234, stdoutToServer=True, stderrToServer=True)
+# import pydevd
+# pydevd.settrace('localhost', port=51234, stdoutToServer=True, stderrToServer=True)
 
 __author__ = "Shahin Zibaee"
 __copyright__ = "Copyright 2018, The Switch lab, KU Leuven"
@@ -44,7 +44,7 @@ Paths.set_up_paths(use_cluster=(len(sys.argv) > 1 and sys.argv[1].strip(' ') == 
 constructor.
 """
 operations = {Str.OPER_RUN_MUT_FSTA.value: False, Str.OPER_RUN_AGDR.value: False, Str.OPER_RUN_FX_RPR.value: False,
-              Str.OPER_RUN_FX_BM.value: True, Str.OPER_RUN_FX_STAB.value: False, Str.OPER_RUN_FX_AC.value: False}
+              Str.OPER_RUN_FX_BM.value: False, Str.OPER_RUN_FX_STAB.value: False, Str.OPER_RUN_FX_AC.value: True}
 
 """
 3. Choose multithreading.
@@ -104,18 +104,24 @@ if operations[Str.OPER_RUN_FX_BM.value]:
     fx = FoldX()
     path_output_bm_pdb_fxmutant_dirs = []
     for path_pdbfile in path_pdbfiles:
-        pdbname = os.path.basename(path_pdbfile)
+        pdbfile = os.path.basename(path_pdbfile)
+        pdbname = pdbfile.split('.')[0]
         for specific_fxmutant in specific_fxmutants:
+            if specific_fxmutant[0] == specific_fxmutant[len(specific_fxmutant) - 1]:
+                print('Mutations to wild-type residues are currently not being performed, therefore the results directory '
+                      'should not exist for this: ' + str(pdbname) + '_' + specific_fxmutant)
+                continue
             path_output_bm_pdb_fxmutant_dirs.append(os.path.join(Paths.OUTPUT_BM, pdbname, specific_fxmutant))
         if not specific_fxmutants:
             path_output_bm_pdb_fxmutant_dirs = glob.glob(os.path.join(Paths.OUTPUT_BM, pdbname, '*'))
         for path_output_bm_pdb_fxmutant_dir in path_output_bm_pdb_fxmutant_dirs:
+            os.chdir(path_output_bm_pdb_fxmutant_dir)
             bm = fx.BuildModel(Cond.INCELL_MAML_FX.value)
             if bm.has_already_generated_avg_bm_fxoutfile(path_output_bm_pdb_fxmutant_dir):
                 fxmutantname = os.path.basename(path_output_bm_pdb_fxmutant_dir)
                 if fx.has_already_removed_config_logs_fxoutfile(path_output_bm_pdb_fxmutant_dir):
-                    print('No configs, logs, or unnecessary fxoutfiles found in ' + pdbname + '_' + fxmutantname + '. Hence, '
-                          'nothing to delete.')
+                    print('No configs, logs, or unnecessary fxoutfiles found in ' + str(pdbname) + '_' + fxmutantname +
+                          '. Hence, nothing to delete.')
                     continue
                 if using_cluster:
                     path_jobq_dir = GUM.os_makedirs(Paths.CONFIG_BM_JOBQ, pdbname, fxmutantname)
@@ -133,19 +139,25 @@ if operations[Str.OPER_RUN_FX_AC.value]:
     path_output_bm_pdb_fxmutant_dirs = []
     path_output_ac_pdb_fxmutant_dirs = []
     for path_pdbfile in path_pdbfiles:
-        pdbname = os.path.basename(path_pdbfile)
+        pdbfile = os.path.basename(path_pdbfile)
+        pdbname = pdbfile.split('.')[0]
         for specific_fxmutant in specific_fxmutants:
+            if specific_fxmutant[0] == specific_fxmutant[len(specific_fxmutant) - 1]:
+                print('Computations on for wild-type residues are currently not being performed, therefore the results '
+                      'directory should not exist for this: ' + str(pdbname) + '_' + specific_fxmutant)
+                continue
             path_output_bm_pdb_fxmutant_dirs.append(os.path.join(Paths.OUTPUT_BM, pdbname, specific_fxmutant))
             path_output_ac_pdb_fxmutant_dirs.append(os.path.join(Paths.OUTPUT_AC, pdbname, specific_fxmutant))
         if not specific_fxmutants:
             path_output_ac_pdb_fxmutant_dirs = glob.glob(os.path.join(Paths.OUTPUT_AC, pdbname, '*'))
         for path_output_ac_pdb_fxmutant_dir in path_output_ac_pdb_fxmutant_dirs:
+            os.chdir(path_output_ac_pdb_fxmutant_dir)
             ac = fx.AnalyseComplex(Cond.INCELL_MAML_FX.value)
             if ac.has_already_generated_summary_ac_fxoutfile(path_output_ac_pdb_fxmutant_dir):
                 fxmutantname = os.path.basename(path_output_ac_pdb_fxmutant_dir)
                 if fx.has_already_removed_config_logs_fxoutfile(path_output_ac_pdb_fxmutant_dir):
-                    print('No configs, logs, or unnecessary fxoutfiles found in ' + pdbname + '_' + fxmutantname + '. Hence, '
-                          'nothing to delete.')
+                    print('No configs, logs, or unnecessary fxoutfiles found in ' + str(pdbname) + '_' + fxmutantname +
+                          '. Hence, nothing to delete.')
                 if using_cluster:
                     path_jobq_dir = GUM.os_makedirs(Paths.CONFIG_BM_JOBQ, pdbname, fxmutantname)
                     Cluster.write_job_q_bash(jobname=Paths.PREFIX_FX_RM.value + fxmutantname, path_job_q_dir=path_jobq_dir,
@@ -161,4 +173,4 @@ if operations[Str.OPER_RUN_FX_AC.value]:
                     ac.rm_all_sumry_except_1_0(path_output_ac_pdb_fxmutant_dir)
 
 
-pydevd.stoptrace()
+# pydevd.stoptrace()
