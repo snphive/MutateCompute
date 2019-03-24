078ea94d2c23b8a6448683ebff9ed3f36e7289e7
Shahin Z <shahin.zibaee@cyance.com>
2019-03-24 19:06:02 +0000

    Add requirements.txt, created first by listing package names without version numbers, using pip install -r requirements.txt. Then in reverse, pip freeze > requirements.txt wrote out their version numbers.

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/u0120577/PycharmProjects/MutateCompute/venv"
+set -gx VIRTUAL_ENV "/Users/shahinzibaee/PycharmProjects/MutateCompute/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
